64d97dba912f46e9d822dc0360ce01931e099f6e
tadas <Unknown>
2008-11-08 07:48:28 +0000

    Better divide applet code

---
+++ common/blueman/gui/CommonUi.py
@@ -0,0 +1,57 @@
+# Copyright (C) 2008 Valmantas Paliksa <walmis at balticum-tv dot lt>
+# Copyright (C) 2008 Tadas Dailyda <tadas at dailyda dot com>
+#
+# Licensed under the GNU General Public License Version 3
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+
+from subprocess import *
+
+from blueman.Constants import *
+from blueman.Functions import *
+import gettext
+_ = gettext.gettext
+
+import gtk
+
+
+def show_about_dialog(app_name):
+	def uri_open(uri):
+		Popen(['xdg-open', uri], stdout=PIPE)
+
+	def email_hook(dialog, email, black_hole):
+		uri_open('mailto:'+email)
+		
+	def url_hook(dialog, url, black_hole):
+		uri_open(url)
+	
+	gtk.about_dialog_set_email_hook(email_hook, None)
+	gtk.about_dialog_set_url_hook(url_hook, None)
+	
+	about = gtk.AboutDialog()
+	about.set_name(app_name)
+	about.set_version(VERSION)
+	about.set_copyright('Copyright \xc2\xa9 2008 Valmantas Paliksa\n'\
+						'Copyright \xc2\xa9 2008 Tadas Dailyda')
+	about.set_comments(_('Blueman is GTK based Bluetooth manager'))
+	about.set_website(WEBSITE)
+	about.set_icon(get_icon('blueman'))
+	about.set_logo(get_icon('blueman', 48))
+	authors = ['Valmantas Paliksa <walmis@balticum-tv.lt>',
+				'Tadas Dailyda <tadas@dailyda.com>']
+	about.set_authors(authors)
+	about.run()
+	about.destroy()
+
