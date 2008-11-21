234416be89451d8839afae9cec614a3ae643a16c
tadas <Unknown>
2008-11-21 16:27:48 +0000

    autogen

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PROJECT="bluez-gnome"
+PROJECT="blueman"
 
 (test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory \"\'$srcdir\'\" does not look like the"
