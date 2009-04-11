4cee2118da22be9987fd07bad44173f038d7135f
walmis <Unknown>
2009-04-11 09:32:12 +0000

    fixed deprecation warning

--- blueman/bluez/Manager.py
+++ blueman/bluez/Manager.py
@@ -38,7 +38,7 @@ class Manager(BaseInterface):
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super(Manager, cls).__new__(cls, *args, **kwargs)
+            cls._instance = super(Manager, cls).__new__(cls)
         return cls._instance
     # __new__
 
