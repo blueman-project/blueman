b782aad3e6194742fc485af08a3c6f978dc8d235
walmis <Unknown>
2009-10-15 18:06:36 +0000

    cleanup debug print

--- blueman/plugins/applet/StatusIcon.py
+++ blueman/plugins/applet/StatusIcon.py
@@ -51,12 +51,11 @@ class StatusIcon(AppletPlugin, gtk.StatusIcon):
 	
 	def Query(self):
 		if not self.Applet.Manager:
-			dprint("manager dead")
 			self.props.visible = False
 			return
 			
 		rets = self.Applet.Plugins.Run("on_query_status_icon_visibility")
-		dprint(rets)
+
 		if not StatusIcon.FORCE_SHOW in rets:
 			if StatusIcon.FORCE_HIDE in rets:
 				self.props.visible = False
