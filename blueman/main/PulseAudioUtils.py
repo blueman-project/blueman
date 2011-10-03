05639182d9b98b6a673d9524793172f883884b06
Valmantas Palikša <walmis@balticum-tv.lt>
2011-10-03 21:23:16 +0300

    Fixed pulseaudio related segfaults

--- blueman/main/PulseAudioUtils.py
+++ blueman/main/PulseAudioUtils.py
@@ -1,8 +1,19 @@
 from ctypes import *
 import gobject
 import weakref
-from blueman.Functions import YELLOW
+from blueman.Functions import YELLOW, dprint
 
+libpulse = CDLL("libpulse.so.0")
+libpulse_glib = CDLL("libpulse-mainloop-glib.so.0")
+
+pa_glib_mainloop_new = libpulse_glib.pa_glib_mainloop_new
+pa_glib_mainloop_new.argtypes = [c_void_p]
+pa_glib_mainloop_new.restype = c_void_p
+
+pa_glib_mainloop_get_api = libpulse_glib.pa_glib_mainloop_get_api
+pa_glib_mainloop_get_api.restype = c_void_p
+pa_glib_mainloop_get_api.argtypes = [c_void_p]
+				
 PA_CONTEXT_UNCONNECTED =  0
 PA_CONTEXT_CONNECTING	= 1
 PA_CONTEXT_AUTHORIZING	= 2
@@ -27,9 +38,6 @@ class EventType:
 	REMOVE = 0x0020 
 	TYPE_MASK = 0x0030
 	
-pa_context_notify_cb_t = CFUNCTYPE(None, c_void_p, py_object)
-pa_context_index_cb_t = CFUNCTYPE(None, c_void_p, c_int, py_object)
-
 class NullError(Exception):
 	pass
 	
@@ -40,7 +48,7 @@ class pa_module_info(Structure):
 	_fields_ = [("index", c_uint),
                     ("name", c_char_p),
                     ("argument", c_char_p),
-                    ("n_used", c_uint),
+                    ("n_used", c_int),
                     ("proplist", c_void_p),
                     ]
 
@@ -87,9 +95,36 @@ pa_source_info._fields_ = [
     ('configured_latency', c_ulong),
     #('base_volume', pa_volume_t),
     #('state', pa_source_state_t),
-    #('n_volume_steps', uint32_t),
-    #('card', uint32_t),
-    #('n_ports', uint32_t),
+    #('n_volume_steps', c_uint32),
+    #('card', c_uint32),
+    #('n_ports', c_uint32),
+    #('ports', POINTER(POINTER(pa_source_port_info))),
+   # ('active_port', POINTER(pa_source_port_info)),
+]
+
+class pa_sink_info(Structure):
+    pass
+pa_sink_info._fields_ = [
+    ('name', c_char_p),
+    ('index', c_uint32),
+    ('description', c_char_p),
+    ('sample_spec', pa_sample_spec),
+    ('channel_map', pa_channel_map),
+    ('owner_module', c_uint32),
+    ('volume', pa_cvolume),
+    ('mute', c_int),
+    ('monitor_of_sink', c_uint32),
+    ('monitor_of_sink_name', c_char_p),
+    ('latency', c_uint64),
+    ('driver', c_char_p),
+    ('flags', c_int),
+    ('proplist', c_void_p),
+    ('configured_latency', c_ulong),
+    #('base_volume', pa_volume_t),
+    #('state', pa_source_state_t),
+    #('n_volume_steps', c_uint32),
+    #('card', c_uint32),
+    #('n_ports', c_uint32),
     #('ports', POINTER(POINTER(pa_source_port_info))),
    # ('active_port', POINTER(pa_source_port_info)),
 ]
@@ -135,15 +170,127 @@ pa_card_info._fields_ = [
     ('active_profile', POINTER(pa_card_profile_info)),
     ('proplist', c_void_p),
 ]
+pa_context_notify_cb_t = CFUNCTYPE(None, c_void_p, py_object)
 
+pa_context_index_cb_t = CFUNCTYPE(None, c_void_p, c_int, py_object)
+pa_context_success_cb_t = pa_context_index_cb_t
 
-
-           
 pa_module_info_cb_t = CFUNCTYPE(None, c_void_p, POINTER(pa_module_info), c_int, py_object)
 pa_source_info_cb_t = CFUNCTYPE(None, c_void_p, POINTER(pa_source_info), c_int, py_object)
 pa_sink_input_info_cb_t = CFUNCTYPE(None, c_void_p, POINTER(pa_sink_input_info), c_int, py_object)
 pa_card_info_cb_t = CFUNCTYPE(None, c_void_p, POINTER(pa_card_info), c_int, py_object)
 pa_context_subscribe_cb_t = CFUNCTYPE(None, c_void_p, c_uint32, c_uint32, c_void_p)
+
+pa_sink_info_cb_t = CFUNCTYPE(None, c_void_p, POINTER(pa_sink_info), c_int, py_object)
+
+
+pa_context_get_module_info_list = libpulse.pa_context_get_module_info_list
+pa_context_get_module_info_list.restype = c_void_p
+pa_context_get_module_info_list.argtypes = [c_void_p, pa_module_info_cb_t, py_object]
+
+pa_context_set_card_profile_by_name = libpulse.pa_context_set_card_profile_by_name
+pa_context_set_card_profile_by_name.restype = c_void_p
+pa_context_set_card_profile_by_name.argtypes = [c_void_p, c_char_p, c_char_p, pa_context_success_cb_t, py_object]
+
+pa_context_set_card_profile_by_index = libpulse.pa_context_set_card_profile_by_index
+pa_context_set_card_profile_by_index.restype = c_void_p
+pa_context_set_card_profile_by_index.argtypes = [c_void_p, c_uint32, c_char_p, pa_context_success_cb_t, py_object]
+
+pa_context_get_card_info_by_index = libpulse.pa_context_get_card_info_by_index
+pa_context_get_card_info_by_index.restype = c_void_p
+pa_context_get_card_info_by_index.argtypes = [c_void_p, c_uint32, pa_card_info_cb_t, py_object]
+
+pa_context_get_card_info_by_name = libpulse.pa_context_get_card_info_by_name
+pa_context_get_card_info_by_name.restype = c_void_p
+pa_context_get_card_info_by_name.argtypes = [c_void_p, c_char_p, pa_card_info_cb_t, py_object]
+
+pa_context_get_card_info_list = libpulse.pa_context_get_card_info_list
+pa_context_get_card_info_list.restype = c_void_p
+pa_context_get_card_info_list.argtypes = [c_void_p, pa_card_info_cb_t, py_object]
+
+pa_context_load_module = libpulse.pa_context_load_module
+pa_context_load_module.restype = c_void_p
+pa_context_load_module.argtypes = [c_void_p, c_char_p, c_char_p, pa_context_index_cb_t, py_object]
+
+pa_context_unload_module = libpulse.pa_context_unload_module
+pa_context_unload_module.restype = c_void_p
+pa_context_unload_module.argtypes = [c_void_p, c_uint32, pa_context_success_cb_t, py_object]
+
+pa_context_move_sink_input_by_index = libpulse.pa_context_move_sink_input_by_index
+pa_context_move_sink_input_by_index.restype = c_void_p
+pa_context_move_sink_input_by_index.argtypes = [c_void_p, c_uint32, c_uint32, pa_context_success_cb_t, py_object]
+
+pa_context_get_sink_input_info_list = libpulse.pa_context_get_sink_input_info_list
+pa_context_get_sink_input_info_list.restype = c_void_p
+pa_context_get_sink_input_info_list.argtypes = [c_void_p, pa_sink_input_info_cb_t, py_object]
+
+pa_context_get_sink_info_list = libpulse.pa_context_get_sink_info_list
+pa_context_get_sink_info_list.restype = c_void_p
+pa_context_get_sink_info_list.argtypes = [c_void_p, pa_sink_info_cb_t, py_object]
+
+pa_context_get_sink_info_list = libpulse.pa_context_get_sink_info_list
+pa_context_get_sink_info_list.restype = c_void_p
+pa_context_get_sink_info_list.argtypes = [c_void_p, pa_sink_info_cb_t, py_object]
+
+pa_context_get_source_info_list = libpulse.pa_context_get_source_info_list
+pa_context_get_source_info_list.restype = c_void_p
+pa_context_get_source_info_list.argtypes = [c_void_p, pa_source_info_cb_t, py_object]
+
+pa_proplist_to_string_sep = libpulse.pa_proplist_to_string_sep
+pa_proplist_to_string_sep.restype = POINTER(c_char)
+pa_proplist_to_string_sep.argtypes = [c_void_p, c_char_p]
+
+pa_context_subscribe = libpulse.pa_context_subscribe
+pa_context_subscribe.restype = c_void_p
+pa_context_subscribe.argtypes = [c_void_p, c_int, pa_context_success_cb_t, py_object]
+
+pa_context_get_state = libpulse.pa_context_get_state
+pa_context_get_state.restype = c_int
+pa_context_get_state.argtypes = [c_void_p]
+
+pa_xfree = libpulse.pa_xfree
+pa_xfree.restype = None
+pa_xfree.argtypes = [c_void_p]
+
+pa_context_disconnect = libpulse.pa_context_disconnect
+pa_context_disconnect.restype = None
+pa_context_disconnect.argtypes = [c_void_p]
+
+pa_context_unref = libpulse.pa_context_unref
+pa_context_unref.restype = None
+pa_context_unref.argtypes = [c_void_p]
+
+pa_operation_unref = libpulse.pa_operation_unref
+pa_operation_unref.restype = None
+pa_operation_unref.argtypes = [c_void_p]
+
+pa_context_set_default_sink = libpulse.pa_context_set_default_sink
+pa_context_set_default_sink.restype = c_void_p
+pa_context_set_default_sink.argtypes = [c_void_p, c_char_p, pa_context_success_cb_t, py_object]
+
+pa_context_set_state_callback = libpulse.pa_context_set_state_callback
+pa_context_set_state_callback.restype = None
+pa_context_set_state_callback.argtypes = [c_void_p, pa_context_notify_cb_t, py_object]
+
+pa_context_connect = libpulse.pa_context_connect
+pa_context_connect.restype = c_int
+pa_context_connect.argtypes = [c_void_p, c_char_p, c_int, c_void_p]
+
+pa_context_set_subscribe_callback = libpulse.pa_context_set_subscribe_callback
+pa_context_set_subscribe_callback.restype = None
+pa_context_set_subscribe_callback.argtypes = [c_void_p, pa_context_subscribe_cb_t, c_void_p]
+
+pa_context_new = libpulse.pa_context_new
+pa_context_new.restype = c_void_p
+pa_context_new.argtypes = [c_void_p, c_char_p]
+
+pa_get_library_version = libpulse.pa_get_library_version
+pa_get_library_version.restype = c_char_p
+pa_get_library_version.argtypes = []
+
+pa_context_errno = libpulse.pa_context_errno
+pa_context_errno.restype = c_int
+pa_context_errno.argtypes = [c_void_p]
 
 class PulseAudioUtils(gobject.GObject):
 	__gsignals__ = {
@@ -161,14 +308,15 @@ class PulseAudioUtils(gobject.GObject):
 		if not self:
 			return
 			
-		state = self.pa.pa_context_get_state(pa_context)
+		state = pa_context_get_state(pa_context)
 		dprint(state)
 		if state == PA_CONTEXT_READY:
 			self.connected = True
 			self.emit("connected")
 			MASK = 0x0200 | 0x0010 #from enum pa_subscription_mask
+			
 			self.simple_callback(lambda x: dprint(x), 
-								 self.pa.pa_context_subscribe,
+								 pa_context_subscribe,
 								 MASK)
 		else:
 			if self.connected:
@@ -183,11 +331,12 @@ class PulseAudioUtils(gobject.GObject):
 		
 	def __get_proplist(self, proplist):
 		if proplist:
-			pla = self.pa.pa_proplist_to_string_sep(proplist, "|")
+			pla = pa_proplist_to_string_sep(proplist, "|")
 			pl = cast(pla, c_char_p)
+
 			ls = pl.value.split("|")
 			del pl
-			self.pa.pa_xfree(pla)
+			pa_xfree(pla)
 		else:
 			ls = []
 		
@@ -212,10 +361,11 @@ class PulseAudioUtils(gobject.GObject):
 		pythonapi.Py_IncRef(py_object(info))
 		
 		args += (info["cb_info"], py_object(info))
-		self.pa.pa_operation_unref(function(self.pa_context, *args))	
+		op = function(self.pa_context, *args)
+		pa_operation_unref(op)	
 		
 	def simple_callback(self, handler, function, *args):
-				
+	
 		def wrapper(context, res, data):
 			if handler:
 				handler(res)
@@ -229,9 +379,9 @@ class PulseAudioUtils(gobject.GObject):
 		if not op:
 			dprint(YELLOW("Operation failed"))
 			print function.__name__
+		pa_operation_unref(op)
+
 		
-		self.pa.pa_operation_unref(op)
-	
 	def ListSources(self, callback):
 		self.check_connected()
 	
@@ -254,7 +404,7 @@ class PulseAudioUtils(gobject.GObject):
 				callback(data)
 		
 		
-		self.__init_list_callback(self.pa.pa_context_get_source_info_list,
+		self.__init_list_callback(pa_context_get_source_info_list,
 					  pa_source_info_cb_t, handler)		
 		
 	
@@ -279,11 +429,11 @@ class PulseAudioUtils(gobject.GObject):
 			if end:
 				callback(data)
 		if id != None:
-			self.__init_list_callback(self.pa.pa_context_get_sink_info_list,
-				  pa_source_info_cb_t, handler, id)	
+			self.__init_list_callback(pa_context_get_sink_info_list,
+				  pa_sink_info_cb_t, handler, id)	
 		else:	
-			self.__init_list_callback(self.pa.pa_context_get_sink_info_list,
-						  pa_source_info_cb_t, handler)
+			self.__init_list_callback(pa_context_get_sink_info_list,
+						  pa_sink_info_cb_t, handler)
 		
 	def ListSinkInputs(self, callback):
 		self.check_connected()
@@ -304,21 +454,21 @@ class PulseAudioUtils(gobject.GObject):
 				"driver": entry_info[0].driver
 			}
 
-		self.__init_list_callback(self.pa.pa_context_get_sink_input_info_list,
+		self.__init_list_callback(pa_context_get_sink_input_info_list,
 					  pa_sink_input_info_cb_t, handler)
 					  
 	def MoveSinkInput(self, input_id, sink_id, callback):
 		self.check_connected()
 		
 		self.simple_callback(callback, 
-							self.pa.pa_context_move_sink_input_by_index, 
+							pa_context_move_sink_input_by_index, 
 							int(input_id), int(sink_id))
 		
 	def SetDefaultSink(self, name, callback):
 		self.check_connected()
 		
-		self.pa.pa_context_load_module.argtypes = (c_void_p, c_char_p, c_void_p, py_object)
-		self.simple_callback(callback, self.pa.pa_context_set_default_sink, name)
+		
+		self.simple_callback(callback, pa_context_set_default_sink, name)
 		
 #CARDS		
 	def __card_info(self, card_info):
@@ -359,7 +509,7 @@ class PulseAudioUtils(gobject.GObject):
 			
 			data[entry["name"]] = entry			
 			
-		self.__init_list_callback(self.pa.pa_context_get_card_info_list,
+		self.__init_list_callback(pa_context_get_card_info_list,
 			  pa_card_info_cb_t, handler)
 			  
 	def GetCard(self, id, callback):
@@ -372,25 +522,26 @@ class PulseAudioUtils(gobject.GObject):
 			callback(self.__card_info(entry_info))
 			
 		if type(id) == str:
-			fn = self.pa.pa_context_get_card_info_by_name
+			fn = pa_context_get_card_info_by_name
 		else:
-			fn = self.pa.pa_context_get_card_info_by_index
+			fn = pa_context_get_card_info_by_index
 			
 		self.__init_list_callback(fn,
 			  pa_card_info_cb_t, handler, id)
 			  
 	def SetCardProfile(self, card_id, profile, callback):
 		if type(card_id) == str:
-			fn = self.pa.pa_context_set_card_profile_by_name
+			fn = pa_context_set_card_profile_by_name
 		else:
-			fn = self.pa.pa_context_set_card_profile_by_index
+			fn = pa_context_set_card_profile_by_index
 					
 		self.simple_callback(callback, fn, card_id, profile)						
 			
 #### Module API #######	
+	#import gtk
 	def ListModules(self, callback):
+
 		self.check_connected()
-		
 		data = {}
 		def handler(entry_info, end):
 			if end:
@@ -405,32 +556,31 @@ class PulseAudioUtils(gobject.GObject):
 				"proplist": props
 			}
 			
-		self.__init_list_callback(self.pa.pa_context_get_module_info_list,
+		self.__init_list_callback(pa_context_get_module_info_list,
 			  pa_module_info_cb_t, handler)
 				
 	def UnloadModule(self, index, callback):
 		self.check_connected()
 			
-		self.simple_callback(callback, self.pa.pa_context_unload_module, index)		
+		self.simple_callback(callback, pa_context_unload_module, index)		
 	
 	def LoadModule(self, name, args, callback):
 		self.check_connected()
 		
 		def handler(res):	
 			if res < 0:
-				callback(-self.pa.pa_context_errno(self.pa_context))
+				callback(-pa_context_errno(self.pa_context))
 			else:
 				callback(res)
 		
-		self.pa.pa_context_load_module.argtypes = (c_void_p, c_char_p, c_char_p, c_void_p, py_object)
 		
-		self.simple_callback(handler, self.pa.pa_context_load_module, name, args)
+		
+		self.simple_callback(handler, pa_context_load_module, name, args)
 		
 #####################	
 
 	def GetVersion(self):
-		self.pa.pa_get_library_version.restype = c_char_p
-		v = self.pa.pa_get_library_version()
+		v = pa_get_library_version()
 		try:
 			a = v.split("-")[0].split(".")
 			a = map(lambda x: int(x), a)
@@ -466,12 +616,9 @@ class PulseAudioUtils(gobject.GObject):
 		
 		self.ctx_cb = pa_context_notify_cb_t(PulseAudioUtils.pa_context_event)
 
-		self.pa = CDLL("libpulse.so.0")
-		self.pa_m = CDLL("libpulse-mainloop-glib.so.0")
-		self.g = CDLL("libglib-2.0.so.0")
+		mainloop = pa_glib_mainloop_new(None)
+		self.pa_mainloop_api = pa_glib_mainloop_get_api(mainloop)
 
-		self.pa_mainloop = self.pa_m.pa_glib_mainloop_new(self.g.g_main_context_default())
-		self.pa_mainloop_api = self.pa_m.pa_glib_mainloop_get_api(self.pa_mainloop)
 		
 		self.pa_context = None
 
@@ -483,21 +630,18 @@ class PulseAudioUtils(gobject.GObject):
 	def Connect(self):
 		if not self.connected:
 			if self.pa_context:
-				self.pa.pa_context_unref(self.pa_context)
+				pa_context_unref(self.pa_context)
 			
-			self.pa_context = self.pa.pa_context_new (self.pa_mainloop_api, "Blueman")
+			self.pa_context = pa_context_new (self.pa_mainloop_api, "Blueman")
 			if not self.pa_context:
 				raise NullError("PA Context returned NULL")
 			
 			self.weak = weakref.proxy(self)
 			
+			pa_context_set_state_callback(self.pa_context, self.ctx_cb, self.weak)
+			pa_context_connect (self.pa_context, None, 0, None)
 			
-			
-			self.pa.pa_context_set_state_callback.argtypes = (c_void_p, pa_context_notify_cb_t, py_object)
-			self.pa.pa_context_set_state_callback(self.pa_context, self.ctx_cb, self.weak)
-			self.pa.pa_context_connect (self.pa_context, None, 0, None)
-			
-			self.pa.pa_context_set_subscribe_callback(self.pa_context,
+			pa_context_set_subscribe_callback(self.pa_context,
 													  self.event_cb,
 													  None)
 													  
@@ -505,8 +649,8 @@ class PulseAudioUtils(gobject.GObject):
 	def __del__(self):
 		dprint("Destroying PulseAudioUtils instance")
 		
-		self.pa.pa_context_disconnect(self.pa_context)
-		self.pa.pa_context_unref(self.pa_context)
+		pa_context_disconnect(self.pa_context)
+		pa_context_unref(self.pa_context)
 		self.pa_context = None
 		
 		del self.ctx_cb
