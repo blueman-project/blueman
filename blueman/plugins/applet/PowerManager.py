333bbce9f9570df48f3ae6f452b625a0974a17a1
Christopher Schramm <cschramm@shakaweb.org>
2013-11-01 09:42:49 +0100

    Reformat blueman/plugins/applet/PowerManager.py

--- blueman/plugins/applet/PowerManager.py
+++ blueman/plugins/applet/PowerManager.py
@@ -15,7 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 from blueman.Functions import *
 from blueman.plugins.AppletPlugin import AppletPlugin
 import blueman.bluez as Bluez
