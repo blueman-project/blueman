45fe0847ae20570ef3dd5cbfd6c49e93e3e6dca6
Christopher Schramm <cschramm@shakaweb.org>
2013-11-10 21:00:33 +0100

    Reformat blueman/plugins/applet/NMIntegration.py

--- blueman/plugins/applet/NMIntegration.py
+++ blueman/plugins/applet/NMIntegration.py
@@ -33,109 +33,111 @@ import dbus
 import gtk
 
 if not HAL_ENABLED:
-	raise ImportError("NMIntegration (deprecated) requires hal support")
+    raise ImportError("NMIntegration (deprecated) requires hal support")
+
 
 class NMIntegration(AppletPlugin):
-	__description__ = _("<b>Deprecated</b>\nMakes DUN/PAN connections available for NetworkManager 0.7")
-	__icon__ = "modem"
-	__depends__ = ["DBusService"]
-	__conflicts__ = ["PPPSupport", "DhcpClient"]
-	__author__ = "Walmis"
-	if HAL_ENABLED:
-		__priority__ = 2
-	
-	def on_load(self, applet):
-		self.Signals = SignalTracker()
-			
-		self.Signals.Handle("dbus", dbus.SystemBus(), 
-						self.on_network_prop_changed, 
-						"PropertyChanged",
-						"org.bluez.Network",
-						path_keyword="path")
-		
-	def on_unload(self):
-		self.Signals.DisconnectAll()
-		
-	def on_network_prop_changed(self, key, value, path):
-		if key == "Device":
-			if value != "":
-				m = Mechanism()
-				m.HalRegisterNetDev(value)
+    __description__ = _("<b>Deprecated</b>\nMakes DUN/PAN connections available for NetworkManager 0.7")
+    __icon__ = "modem"
+    __depends__ = ["DBusService"]
+    __conflicts__ = ["PPPSupport", "DhcpClient"]
+    __author__ = "Walmis"
+    if HAL_ENABLED:
+        __priority__ = 2
 
-	#in: bluez_device_path, rfcomm_device
-	#@dbus.service.method(dbus_interface='org.blueman.Applet', in_signature="ss", out_signature="")
-	def RegisterModem(self, device_path, rfcomm_device):
-		dev = Bluez.Device(device_path)
-		props = dev.GetProperties()
-	
-		m = Mechanism()
+    def on_load(self, applet):
+        self.Signals = SignalTracker()
 
-		def reply():
-			dprint("Registered modem")
-			
-		def err(excp):
-			d = gtk.MessageDialog(None, type=gtk.MESSAGE_WARNING)
-			d.props.icon_name = "blueman"
-			d.props.text = _("CDMA or GSM not supported")
-			d.props.secondary_text = _("The device %s does not appear to support GSM/CDMA.\nThis connection will not work.") % props["Alias"]
+        self.Signals.Handle("dbus", dbus.SystemBus(),
+                            self.on_network_prop_changed,
+                            "PropertyChanged",
+                            "org.bluez.Network",
+                            path_keyword="path")
 
-			d.add_button(gtk.STOCK_OK, gtk.RESPONSE_NO)
-			resp = d.run()
-			d.destroy()
+    def on_unload(self):
+        self.Signals.DisconnectAll()
 
-		m.HalRegisterModemPort(rfcomm_device, props["Address"], reply_handler=reply, error_handler=err)
+    def on_network_prop_changed(self, key, value, path):
+        if key == "Device":
+            if value != "":
+                m = Mechanism()
+                m.HalRegisterNetDev(value)
 
-			
-		
-	#in: bluez_device_path, rfcomm_device
-	#@dbus.service.method(dbus_interface='org.blueman.Applet', in_signature="s", out_signature="")
-	def UnregisterModem(self, device):
-		m = Mechanism()
-		m.HalUnregisterModemPortDev(device)
-			
-		dprint("Unregistered modem")
-		
-	def on_rfcomm_connected(self, device, port, uuid):
-		signals = SignalTracker()
-		def modem_added(mon, udi, address):
-			if device.Address == address:
-				dprint(udi)
-				device.udi = udi
-			
-		def modem_removed(mon, udi):
-			if device.udi == udi:
-				dprint(udi)
-				signals.DisconnectAll()
-			
-		def disconnected(mon, udi):
-			device.Services["serial"].Disconnect(port)
-			self.UnregisterModem(port)
-		
-		def device_propery_changed(key, value):
-			if key == "Connected" and not value:
-				self.UnregisterModem(port)
-			
-	
-		uuid16 = sdp_get_serial_type(device.Address, uuid)
-		if DIALUP_NET_SVCLASS_ID in uuid16:
-			try:
-				signals.Handle(self.Applet.Plugins.NMMonitor, "modem-added", modem_added)
-				signals.Handle(self.Applet.Plugins.NMMonitor, "modem-removed", modem_removed)
-				signals.Handle(self.Applet.Plugins.NMMonitor, "disconnected", disconnected)
-			except KeyError:
-				pass
-				
-			signals.Handle("bluez", device.Device, device_propery_changed, "PropertyChanged")	
-			self.RegisterModem(device.get_object_path(), port)
-		
-	def rfcomm_connect_handler(self, device, uuid, reply_handler, error_handler):
-		uuid16 = sdp_get_serial_type(device.Address, uuid)
-		if DIALUP_NET_SVCLASS_ID in uuid16:
-			device.Services["serial"].Connect(uuid, reply_handler=reply_handler, error_handler=error_handler)
-			return True
-		else:
-			return False	
-		
-		
-	def on_rfcomm_disconnect(self, port):
-		self.UnregisterModem(port)
+    #in: bluez_device_path, rfcomm_device
+    #@dbus.service.method(dbus_interface='org.blueman.Applet', in_signature="ss", out_signature="")
+    def RegisterModem(self, device_path, rfcomm_device):
+        dev = Bluez.Device(device_path)
+        props = dev.GetProperties()
+
+        m = Mechanism()
+
+        def reply():
+            dprint("Registered modem")
+
+        def err(excp):
+            d = gtk.MessageDialog(None, type=gtk.MESSAGE_WARNING)
+            d.props.icon_name = "blueman"
+            d.props.text = _("CDMA or GSM not supported")
+            d.props.secondary_text = _(
+                "The device %s does not appear to support GSM/CDMA.\nThis connection will not work.") % props["Alias"]
+
+            d.add_button(gtk.STOCK_OK, gtk.RESPONSE_NO)
+            resp = d.run()
+            d.destroy()
+
+        m.HalRegisterModemPort(rfcomm_device, props["Address"], reply_handler=reply, error_handler=err)
+
+
+    #in: bluez_device_path, rfcomm_device
+    #@dbus.service.method(dbus_interface='org.blueman.Applet', in_signature="s", out_signature="")
+    def UnregisterModem(self, device):
+        m = Mechanism()
+        m.HalUnregisterModemPortDev(device)
+
+        dprint("Unregistered modem")
+
+    def on_rfcomm_connected(self, device, port, uuid):
+        signals = SignalTracker()
+
+        def modem_added(mon, udi, address):
+            if device.Address == address:
+                dprint(udi)
+                device.udi = udi
+
+        def modem_removed(mon, udi):
+            if device.udi == udi:
+                dprint(udi)
+                signals.DisconnectAll()
+
+        def disconnected(mon, udi):
+            device.Services["serial"].Disconnect(port)
+            self.UnregisterModem(port)
+
+        def device_propery_changed(key, value):
+            if key == "Connected" and not value:
+                self.UnregisterModem(port)
+
+
+        uuid16 = sdp_get_serial_type(device.Address, uuid)
+        if DIALUP_NET_SVCLASS_ID in uuid16:
+            try:
+                signals.Handle(self.Applet.Plugins.NMMonitor, "modem-added", modem_added)
+                signals.Handle(self.Applet.Plugins.NMMonitor, "modem-removed", modem_removed)
+                signals.Handle(self.Applet.Plugins.NMMonitor, "disconnected", disconnected)
+            except KeyError:
+                pass
+
+            signals.Handle("bluez", device.Device, device_propery_changed, "PropertyChanged")
+            self.RegisterModem(device.get_object_path(), port)
+
+    def rfcomm_connect_handler(self, device, uuid, reply_handler, error_handler):
+        uuid16 = sdp_get_serial_type(device.Address, uuid)
+        if DIALUP_NET_SVCLASS_ID in uuid16:
+            device.Services["serial"].Connect(uuid, reply_handler=reply_handler, error_handler=error_handler)
+            return True
+        else:
+            return False
+
+
+    def on_rfcomm_disconnect(self, port):
+        self.UnregisterModem(port)
