da332df5884a0563b3f9c3d4963f350cc016617b
Christopher Schramm <cschramm@shakaweb.org>
2014-04-21 11:58:45 +0200

    Reformat blueman/gui/GenericList.py

--- blueman/gui/GenericList.py
+++ blueman/gui/GenericList.py
@@ -1,188 +1,147 @@
-# Copyright (C) 2008 Valmantas Paliksa <walmis at balticum-tv dot lt>
-# Copyright (C) 2008 Tadas Dailyda <tadas at dailyda dot com>
-#
-# Licensed under the GNU General Public License Version 3
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
-
 from gi.repository import Gtk
 from gi.repository import GObject
 
 
+# noinspection PyAttributeOutsideInit
 class GenericList(Gtk.TreeView):
+    def __init__(self, data):
+        GObject.GObject.__init__(self)
+        self.selection = self.get_selection()
+        self._load(data)
 
-	def __init__(self, data):
-		GObject.GObject.__init__(self)
-		self.selection = self.get_selection()
-		self._load(data)
-		
-		
+    def _load(self, data):
+        self.ids = {}
+        self.columns = {}
 
-	def _load(self, data):
-		
-		self.ids = {}
-		self.columns = {}
-		
+        types = []
+        for i in range(len(data)):
+            types.append(data[i][1])
 
-		types = []
-		for i in range(len(data)):
-			types.append(data[i][1])
-			
-		types = tuple(types)
-		
-		self.liststore = Gtk.ListStore(*types)
-		self.set_model(self.liststore)
-		column = None
-		for i in range(len(data)):
+        types = tuple(types)
 
-			self.ids[data[i][0]] = i
-			
-			
-			if len(data[i]) == 5 or len(data[i]) == 6:
+        self.liststore = Gtk.ListStore(*types)
+        self.set_model(self.liststore)
 
-				column = Gtk.TreeViewColumn(data[i][4])
-				
-				column.pack_start(data[i][2], True)
-				column.set_attributes(data[i][2], **data[i][3])				
-				
-				
-				if len(data[i]) == 6:
-					column.set_properties(**data[i][5])
-				
-				self.columns[data[i][0]] = column
-				self.append_column(column)
-				
+        for i in range(len(data)):
 
-    
-	def selected(self):
-		(model, iter) = self.selection.get_selected()
-               
-               	return iter
+            self.ids[data[i][0]] = i
 
-                	
-    
-    	def delete(self, id):
-		if type(id) == Gtk.TreeIter:
-			iter = id
-		else:
-			iter = self.get_iter(id)
-    		
-    		if iter == None:
-    			return False
-    		if self.liststore.iter_is_valid(iter):
-    			self.liststore.remove(iter)
-    			return True
-    		else:
-    			return False
-    			
-    	
-    	def _add(self, **columns):
-    		ids_len = len(self.ids)
-    		cols_len = len(columns)
-    		
-		items = {}
-    		for k, v in self.ids.iteritems():
-    			items[v] = None
-	
- 		
-    		for k, v in columns.iteritems():
-    			if k in self.ids:
-    				items[self.ids[k]] = v
-    			else:
-    				raise Exception, "Invalid key %s" % k
-    				
-		return items.values()
-		
-	def append(self, **columns):
-		vals = self._add(**columns)
-		return self.liststore.append(vals)
-	
-	def prepend(self, **columns):
-		vals = self._add(**columns)
-		return self.liststore.prepend(vals)
-    		
-	def get_conditional(self, **cols):
-   		ret = []
-   		matches = 0
-   		for i in range(len(self.liststore)):
-   			row = self.get(i)
-    			for k, v in cols.iteritems():
-    				if row[k] == v:
-    					matches += 1
-    					
-    			if matches == len(cols):
-    				ret.append(i)
-    				
-    			matches = 0
+            if len(data[i]) == 5 or len(data[i]) == 6:
 
-    		return ret
-    		
-    		
-    	def set(self, id, **cols):
-		if type(id) == Gtk.TreeIter:
-			iter = id
-		else:
-			iter = self.get_iter(id)
-		
-		if iter != None:
-	    		for k, v in cols.iteritems():
-	    			self.liststore.set(iter, self.ids[k], v)
-	    		return True
-		else:
-	    		return False
+                column = Gtk.TreeViewColumn(data[i][4])
 
-    	
-    	
-    	def get(self, id, *items):
-		ret = {}
+                column.pack_start(data[i][2], True)
+                column.set_attributes(data[i][2], **data[i][3])
 
-		if id != None:
-			if type(id) == Gtk.TreeIter:
-				iter = id
-			else:
-				iter = self.get_iter(id)
-			if len(items) == 0:
-				for k, v in self.ids.iteritems():
-					ret[k] = self.liststore.get(iter, v)[0]
-			else:
-				for i in range(len(items)):
-					if items[i] in self.ids:
-						ret[items[i]] = self.liststore.get(iter, self.ids[items[i]])[0]
-		else:
-			return False
+                if len(data[i]) == 6:
+                    column.set_properties(**data[i][5])
 
-	
-		return ret
+                self.columns[data[i][0]] = column
+                self.append_column(column)
 
-        	
-  	
-    	def get_iter(self, path):
-        	if path == None:
-        		return None
-        	
-        	try:
-            		return self.liststore.get_iter(path)
-        	except:
-            		return None
-            		
-	def clear(self):
-		self.liststore.clear()
-		
-	def compare(self, iter_a, iter_b):
-		if iter_a != None and iter_b != None:
-			return self.get_model().get_path(iter_a) == self.get_model().get_path(iter_b)
-		else:
-			return False
+    def selected(self):
+        (model, iter) = self.selection.get_selected()
 
+        return iter
+
+    def delete(self, id):
+        if type(id) == Gtk.TreeIter:
+            iter = id
+        else:
+            iter = self.get_iter(id)
+
+        if iter is None:
+            return False
+        if self.liststore.iter_is_valid(iter):
+            self.liststore.remove(iter)
+            return True
+        else:
+            return False
+
+    def _add(self, **columns):
+        items = {}
+        for k, v in self.ids.items():
+            items[v] = None
+
+        for k, v in columns.items():
+            if k in self.ids:
+                items[self.ids[k]] = v
+            else:
+                raise Exception("Invalid key %s" % k)
+
+        return items.values()
+
+    def append(self, **columns):
+        vals = self._add(**columns)
+        return self.liststore.append(vals)
+
+    def prepend(self, **columns):
+        vals = self._add(**columns)
+        return self.liststore.prepend(vals)
+
+    def get_conditional(self, **cols):
+        ret = []
+        matches = 0
+        for i in range(len(self.liststore)):
+            row = self.get(i)
+            for k, v in cols.items():
+                if row[k] == v:
+                    matches += 1
+
+            if matches == len(cols):
+                ret.append(i)
+
+            matches = 0
+
+        return ret
+
+    def set(self, id, **cols):
+        if type(id) == Gtk.TreeIter:
+            iter = id
+        else:
+            iter = self.get_iter(id)
+
+        if iter is not None:
+            for k, v in cols.items():
+                self.liststore.set(iter, self.ids[k], v)
+            return True
+        else:
+            return False
+
+    def get(self, id, *items):
+        ret = {}
+
+        if id is not None:
+            if type(id) == Gtk.TreeIter:
+                iter = id
+            else:
+                iter = self.get_iter(id)
+            if len(items) == 0:
+                for k, v in self.ids.items():
+                    ret[k] = self.liststore.get(iter, v)[0]
+            else:
+                for i in range(len(items)):
+                    if items[i] in self.ids:
+                        ret[items[i]] = self.liststore.get(iter, self.ids[items[i]])[0]
+        else:
+            return False
+
+        return ret
+
+    def get_iter(self, path):
+        if path is None:
+            return None
+
+        try:
+            return self.liststore.get_iter(path)
+        except:
+            return None
+
+    def clear(self):
+        self.liststore.clear()
+
+    def compare(self, iter_a, iter_b):
+        if iter_a is not None and iter_b is not None:
+            return self.get_model().get_path(iter_a) == self.get_model().get_path(iter_b)
+        else:
+            return False
