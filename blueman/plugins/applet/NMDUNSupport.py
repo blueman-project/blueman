c94a55ebd91a0d08e763ac650efe94cf025f51ea
Christopher Schramm <cschramm@shakaweb.org>
2014-10-07 14:22:26 +0200

    Reformat NMDUNSupport and PPPSupport

--- blueman/plugins/applet/NMDUNSupport.py
+++ blueman/plugins/applet/NMDUNSupport.py
@@ -1,122 +1,105 @@
-# Copyright (C) 2009 Valmantas Paliksa <walmis at balticum-tv dot lt>
-#
-# Licensed under the GNU General Public License Version 3
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from blueman.plugins.AppletPlugin import AppletPlugin
 import dbus
 from gi.repository import GObject
 from blueman.main.SignalTracker import SignalTracker
 from blueman.gui.Notification import Notification
 from blueman.Sdp import *
-from blueman.Functions import get_icon, composite_icon
+from blueman.Functions import get_icon, composite_icon, dprint
 import weakref
 
-class ConnectionHandler:
-	def __init__(self, parent, device, uuid, reply, err):
-		self.parent = parent
-		self.device = device
-		self.uuid = uuid
-		self.reply = reply
-		self.err = err
-		self.rfcomm_dev = None
-		self.timeout = None
-		
-		self.signals = SignalTracker()
-		self.signals.Handle("dbus", self.parent.bus, 
-				    self.on_mm_device_added, 
-				    "DeviceAdded", 
-				    "org.freedesktop.ModemManager")
-				    
-		#for some reason these handlers take a reference and don't give it back
-		#so i have to workaround :(
-		w = weakref.ref(self)
-		device.Services["serial"].Connect(uuid, 
-						  reply_handler=lambda *args: w() and w().on_connect_reply(*args), 
-						  error_handler=lambda *args: w() and w().on_connect_error(*args))
-		
-	def __del__(self):
-		dprint("deleting")
-		
-	def on_connect_reply(self, rfcomm):
-		self.rfcomm_dev = rfcomm
-		self.timeout = GObject.timeout_add(10000, self.on_timeout)
-		
-	def on_connect_error(self, *args):
-		self.err(*args)
-		self.cleanup()
-		
-	def cleanup(self):
-		if self.timeout:
-			GObject.source_remove(self.timeout)
-		self.signals.DisconnectAll()
-		
-		del self.device
-		
-	def on_mm_device_added(self, path):
-		dprint(path)
-		props = self.parent.bus.call_blocking("org.freedesktop.ModemManager", 
-						path, 
-						"org.freedesktop.DBus.Properties", 
-						"GetAll", 
-						"s", 
-						["org.freedesktop.ModemManager.Modem"])
-						
-		if self.rfcomm_dev and props["Driver"] == "bluetooth" and props["Device"] in self.rfcomm_dev:
-			dprint("It's our bluetooth modem!")
-			
-			modem = get_icon("modem", 24)
-			blueman = get_icon("blueman", 48)
-			
-			icon = composite_icon(blueman, [(modem, 24, 24, 255)])
-			
-			Notification(_("Bluetooth Dialup"), 
-				     _("DUN connection on %s will now be available in Network Manager") % self.device.Alias, 
-				     pixbuf=icon,
-				     status_icon=self.parent.Applet.Plugins.StatusIcon)
-				     
-			self.reply(self.rfcomm_dev)
-			self.cleanup()
 
-			
-	def on_timeout(self):
-		self.timeout = None
-		self.err(dbus.DBusException(_("Modem Manager did not support the connection")))
-		self.cleanup()
+class ConnectionHandler:
+    def __init__(self, parent, device, uuid, reply, err):
+        self.parent = parent
+        self.device = device
+        self.uuid = uuid
+        self.reply = reply
+        self.err = err
+        self.rfcomm_dev = None
+        self.timeout = None
+
+        self.signals = SignalTracker()
+        self.signals.Handle("dbus", self.parent.bus,
+                            self.on_mm_device_added,
+                            "DeviceAdded",
+                            "org.freedesktop.ModemManager")
+
+        # for some reason these handlers take a reference and don't give it back
+        #so i have to workaround :(
+        w = weakref.ref(self)
+        device.Services["serial"].Connect(uuid,
+                                          reply_handler=lambda *args: w() and w().on_connect_reply(*args),
+                                          error_handler=lambda *args: w() and w().on_connect_error(*args))
+
+    def __del__(self):
+        dprint("deleting")
+
+    def on_connect_reply(self, rfcomm):
+        self.rfcomm_dev = rfcomm
+        self.timeout = GObject.timeout_add(10000, self.on_timeout)
+
+    def on_connect_error(self, *args):
+        self.err(*args)
+        self.cleanup()
+
+    def cleanup(self):
+        if self.timeout:
+            GObject.source_remove(self.timeout)
+        self.signals.DisconnectAll()
+
+        del self.device
+
+    def on_mm_device_added(self, path):
+        dprint(path)
+        props = self.parent.bus.call_blocking("org.freedesktop.ModemManager",
+                                              path,
+                                              "org.freedesktop.DBus.Properties",
+                                              "GetAll",
+                                              "s",
+                                              ["org.freedesktop.ModemManager.Modem"])
+
+        if self.rfcomm_dev and props["Driver"] == "bluetooth" and props["Device"] in self.rfcomm_dev:
+            dprint("It's our bluetooth modem!")
+
+            modem = get_icon("modem", 24)
+            blueman = get_icon("blueman", 48)
+
+            icon = composite_icon(blueman, [(modem, 24, 24, 255)])
+
+            Notification(_("Bluetooth Dialup"),
+                         _("DUN connection on %s will now be available in Network Manager") % self.device.Alias,
+                         pixbuf=icon,
+                         status_icon=self.parent.Applet.Plugins.StatusIcon)
+
+            self.reply(self.rfcomm_dev)
+            self.cleanup()
+
+    def on_timeout(self):
+        self.timeout = None
+        self.err(dbus.DBusException(_("Modem Manager did not support the connection")))
+        self.cleanup()
+
 
 class NMDUNSupport(AppletPlugin):
-	__depends__ = ["StatusIcon", "DBusService"]
-	__conflicts__ = ["PPPSupport", "NMIntegration"]
-	__icon__ = "modem"
-	__author__ = "Walmis"
-	__description__ = _("Provides support for Dial Up Networking (DUN) with ModemManager and NetworkManager 0.8")
-	__priority__ = 1
-	
-	def on_load(self, applet):
-		self.bus = dbus.SystemBus()
-		
-	def on_unload(self):
-		pass
-		
-	def rfcomm_connect_handler(self, device, uuid, reply, err):
-		uuid16 = sdp_get_serial_type(device.Address, uuid)
-		if DIALUP_NET_SVCLASS_ID in uuid16:		
+    __depends__ = ["StatusIcon", "DBusService"]
+    __conflicts__ = ["PPPSupport", "NMIntegration"]
+    __icon__ = "modem"
+    __author__ = "Walmis"
+    __description__ = _("Provides support for Dial Up Networking (DUN) with ModemManager and NetworkManager 0.8")
+    __priority__ = 1
 
-			ConnectionHandler(self, device, uuid, reply, err)
-		
-			return True
-		else:
-			return False
+    def on_load(self, applet):
+        self.bus = dbus.SystemBus()
 
+    def on_unload(self):
+        pass
+
+    def rfcomm_connect_handler(self, device, uuid, reply, err):
+        uuid16 = sdp_get_serial_type(device.Address, uuid)
+        if DIALUP_NET_SVCLASS_ID in uuid16:
+
+            ConnectionHandler(self, device, uuid, reply, err)
+
+            return True
+        else:
+            return False
--- blueman/plugins/applet/PPPSupport.py
+++ blueman/plugins/applet/PPPSupport.py
@@ -1,105 +1,87 @@
-# Copyright (C) 2008 Valmantas Paliksa <walmis at balticum-tv dot lt>
-# Copyright (C) 2008 Tadas Dailyda <tadas at dailyda dot com>
-#
-# Licensed under the GNU General Public License Version 3
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
 from blueman.Functions import *
 from blueman.plugins.AppletPlugin import AppletPlugin
-from blueman.bluez.Device import Device
 from blueman.gui.Notification import Notification
 from blueman.main.Mechanism import Mechanism
-from blueman.main.PPPConnection import PPPConnection
 from blueman.main.Config import Config
 
 from gi.repository import GObject
 
 from blueman.Sdp import *
 import os
-import dbus
 
-class connection:
-		
-	def __init__(self, applet, device, port, ok, err):
-		self.reply_handler = ok
-		self.error_handler = err
-		self.device = device
-		self.port = port
-		self.Applet = applet
-		
-		res = os.popen("ps x -o pid,args | grep modem-manager").read()
-		if not res:
-			self.connect()
-		else:
-			dprint("ModemManager is running, delaying connection 5sec for it to complete probing")
-			GObject.timeout_add(5000, self.connect)
-		
-	def connect(self):
-		c = Config("gsm_settings/" + self.device.Address)
-		if c.props.apn == None:
-			c.props.apn = ""
-			
-		if c.props.number == None:
-			c.props.number = "*99#"	
-		
-		m = Mechanism()
-		m.PPPConnect(self.port, c.props.number, c.props.apn, reply_handler=self.on_connected, error_handler=self.on_error, timeout=200)	
-	
-	def on_error(self, error):
-		self.error_handler(error)
-		GObject.timeout_add(1000, self.device.Services["serial"].Disconnect, self.port)
-		
-	def on_connected(self, iface):
-		self.reply_handler(self.port)
-		self.Applet.Plugins.Run("on_ppp_connected", self.device, self.port, iface)
-		
-		Notification(_("Connected"), _("Successfully connected to <b>DUN</b> service on <b>%(0)s.</b>\nNetwork is now available through <b>%(1)s</b>") % {"0":self.device.Alias, "1":iface}, pixbuf=get_icon("network-wireless", 48), status_icon=self.Applet.Plugins.StatusIcon)	
 
-	
+class Connection:
+    def __init__(self, applet, device, port, ok, err):
+        self.reply_handler = ok
+        self.error_handler = err
+        self.device = device
+        self.port = port
+        self.Applet = applet
+
+        res = os.popen("ps x -o pid,args | grep modem-manager").read()
+        if not res:
+            self.connect()
+        else:
+            dprint("ModemManager is running, delaying connection 5sec for it to complete probing")
+            GObject.timeout_add(5000, self.connect)
+
+    def connect(self):
+        c = Config("gsm_settings/" + self.device.Address)
+        if c.props.apn is None:
+            c.props.apn = ""
+
+        if c.props.number is None:
+            c.props.number = "*99#"
+
+        m = Mechanism()
+        m.PPPConnect(self.port, c.props.number, c.props.apn, reply_handler=self.on_connected,
+                     error_handler=self.on_error, timeout=200)
+
+    def on_error(self, error):
+        self.error_handler(error)
+        GObject.timeout_add(1000, self.device.Services["serial"].Disconnect, self.port)
+
+    def on_connected(self, iface):
+        self.reply_handler(self.port)
+        self.Applet.Plugins.Run("on_ppp_connected", self.device, self.port, iface)
+
+        msg = _("Successfully connected to <b>DUN</b> service on <b>%(0)s.</b>\n"
+                "Network is now available through <b>%(1)s</b>") % {"0": self.device.Alias, "1": iface}
+
+        Notification(_("Connected"), msg, pixbuf=get_icon("network-wireless", 48),
+                     status_icon=self.Applet.Plugins.StatusIcon)
+
 
 class PPPSupport(AppletPlugin):
-	__depends__ = ["DBusService"]
-	__conflicts__ = ["NMIntegration"]
-	__description__ = _("Provides basic support for connecting to the internet via DUN profile.")
-	__author__ = "Walmis"
-	__icon__ = "modem"
-	__priority__ = 0
-	
-	def on_load(self, applet):
-		AppletPlugin.add_method(self.on_ppp_connected)
-		
-	def on_unload(self):
-		pass
-		
-	def on_ppp_connected(self, device, rfcomm, ppp_port):
-		pass
-		
-	def on_rfcomm_connected(self, device, port, uuid):
-		pass
-		
-	def rfcomm_connect_handler(self, device, uuid, reply, err):
-		uuid16 = sdp_get_serial_type(device.Address, uuid)
-		if DIALUP_NET_SVCLASS_ID in uuid16:
-		
-			def local_reply(port):
-				connection(self.Applet, device, port, reply, err)
-				#self.port2device[port] = device
-					
-			device.Services["serial"].Connect(uuid, reply_handler=local_reply, error_handler=err)
-			dprint("Connecting rfcomm device")
-		
-			return True
-		else:
-			return False
+    __depends__ = ["DBusService"]
+    __conflicts__ = ["NMIntegration"]
+    __description__ = _("Provides basic support for connecting to the internet via DUN profile.")
+    __author__ = "Walmis"
+    __icon__ = "modem"
+    __priority__ = 0
+
+    def on_load(self, applet):
+        AppletPlugin.add_method(self.on_ppp_connected)
+
+    def on_unload(self):
+        pass
+
+    def on_ppp_connected(self, device, rfcomm, ppp_port):
+        pass
+
+    def on_rfcomm_connected(self, device, port, uuid):
+        pass
+
+    def rfcomm_connect_handler(self, device, uuid, reply, err):
+        uuid16 = sdp_get_serial_type(device.Address, uuid)
+        if DIALUP_NET_SVCLASS_ID in uuid16:
+
+            def local_reply(port):
+                Connection(self.Applet, device, port, reply, err)
+
+            device.Services["serial"].Connect(uuid, reply_handler=local_reply, error_handler=err)
+            dprint("Connecting rfcomm device")
+
+            return True
+        else:
+            return False
