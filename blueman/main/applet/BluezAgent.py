77976b4cbc88a686689d0ce59a4ab4a7910f1142
Christopher Schramm <git@cschramm.eu>
2015-01-02 17:36:12 +0100

    Add DisplayPasskey / DisplayPinCode methods to CommonAgent
    Closes #120
    Closes #162

--- blueman/main/applet/BluezAgent.py
+++ blueman/main/applet/BluezAgent.py
@@ -167,6 +167,18 @@ class CommonAgent(GObject.GObject, Agent):
         if self.dialog:
             self.dialog.present_with_time(self.time_func())
 
+    @AgentMethod
+    def DisplayPasskey(self, device, passkey, entered):
+        dprint('DisplayPasskey (%s, %d)' % (device, passkey))
+        notify_message = _("Pairing passkey for") + " %s: %s" % (self.get_device_alias(device), passkey)
+        Notification("Bluetooth", notify_message, 0, pixbuf=get_icon("blueman", 48), status_icon=self.status_icon)
+
+    @AgentMethod
+    def DisplayPinCode(self, device, pin_code):
+        dprint('DisplayPinCode (%s, %s)' % (device, pin_code))
+        notify_message = _("Pairing PIN code for") + " %s: %s" % (self.get_device_alias(device), pin_code)
+        Notification("Bluetooth", notify_message, 0, pixbuf=get_icon("blueman", 48), status_icon=self.status_icon)
+
 
 # noinspection PyPep8Naming
 class AdapterAgent(CommonAgent):
