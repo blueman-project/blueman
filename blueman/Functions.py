2c0ca3d50d732e53437d8fde7f8fce6d9ef74fbc
infirit <infirit@gmail.com>
2015-03-05 16:56:21 +0100

    Use the open function from the io module.
    This is the backported io code from python3 and allows us to
    support both versions.

--- blueman/Functions.py
+++ blueman/Functions.py
@@ -21,6 +21,7 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 from __future__ import unicode_literals
+from io import open
 
 from blueman.Constants import *
 
