c1e5a454be5d6a1c0422af87b0bd89fb5148a76a
infirit <infirit@gmail.com>
2015-04-12 19:45:14 +0200

    module: absolute_import causes problems on older machines.
    This future import is only supported with cython 0.17 and up. Debian
    wheezy for example does not have this version yet and as we do not
    import anything we can omit this import for now.
    
    I left in in the code with a comment so others know the reasons.

--- module/_blueman.pyx
+++ module/_blueman.pyx
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from __future__ import division
-from __future__ import absolute_import
+#Comment out this import for cython version < 0.17 (wheezy)
+#We do not import any module here so no real loss.
+#from __future__ import absolute_import
 from __future__ import unicode_literals
 
 cdef extern from "malloc.h":
