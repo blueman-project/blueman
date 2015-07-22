6f19e021ad0fcf0b5ccd452b457b97db7ad727ac
Christopher Schramm <git@cschramm.eu>
2015-07-22 17:24:31 +0200

    Fix unblocking rfkill for Python 3
    Closes #327

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@
 
 * Codepoint issues when connecting to serial service
 * Infinite loop if RecentConns has only invalid items
+* Unblock rfkill in Python 3
 
 ## 2.0
 
--- blueman/plugins/mechanism/RfKill.py
+++ blueman/plugins/mechanism/RfKill.py
@@ -12,6 +12,6 @@ from blueman.plugins.applet.KillSwitch import RFKILL_TYPE_BLUETOOTH, RFKILL_OP_C
 class RfKill(MechanismPlugin):
     @dbus.service.method('org.blueman.Mechanism', in_signature="b", out_signature="")
     def SetRfkillState(self, state):
-        f = open('/dev/rfkill', 'w')
+        f = open('/dev/rfkill', 'wb')
         f.write(struct.pack("IBBBB", 0, RFKILL_TYPE_BLUETOOTH, RFKILL_OP_CHANGE_ALL, (0 if state else 1), 0))
         f.close()
