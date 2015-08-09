5662bc6180c349223878663d162bc74ed01f15c9
Michał Górny <mgorny@gentoo.org>
2015-08-09 13:21:29 +0200

    RfKill: open /dev/rfkill r+b to avoid creating
    Open /dev/rfkill in r+b mode rather than wb since the latter creates the
    file if it does not exist. And we certainly can not create /dev/rfkill
    as a regular file since this is never going to work and is going to
    break a lot of other programs that assume /dev/rfkill is a valid device
    node.
    
    Fixes #333
    Closes #336

--- blueman/plugins/mechanism/RfKill.py
+++ blueman/plugins/mechanism/RfKill.py
@@ -15,6 +15,6 @@ if not os.path.exists('/dev/rfkill'):
 class RfKill(MechanismPlugin):
     @dbus.service.method('org.blueman.Mechanism', in_signature="b", out_signature="")
     def SetRfkillState(self, state):
-        f = open('/dev/rfkill', 'wb')
+        f = open('/dev/rfkill', 'r+b')
         f.write(struct.pack("IBBBB", 0, RFKILL_TYPE_BLUETOOTH, RFKILL_OP_CHANGE_ALL, (0 if state else 1), 0))
         f.close()
