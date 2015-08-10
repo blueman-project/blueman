632f5ca963c292af80bcaa58eee861b68ac56765
infirit <infirit@gmail.com>
2015-08-10 11:31:14 +0200

    Handle when rfkill subsystem is not available gracefully
    Check for the /dev/rfkill device and raise ImportError if not
    found to stop the plugins from loading.

--- blueman/plugins/mechanism/RfKill.py
+++ blueman/plugins/mechanism/RfKill.py
@@ -4,10 +4,13 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 import dbus.service
+import os
 import struct
 from blueman.plugins.MechanismPlugin import MechanismPlugin
 from blueman.plugins.applet.KillSwitch import RFKILL_TYPE_BLUETOOTH, RFKILL_OP_CHANGE_ALL
 
+if not os.path.exists('/dev/rfkill'):
+    raise ImportError("Hardware kill switch not found")
 
 class RfKill(MechanismPlugin):
     @dbus.service.method('org.blueman.Mechanism', in_signature="b", out_signature="")
