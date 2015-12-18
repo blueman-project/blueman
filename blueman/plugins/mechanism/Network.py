e3d249391654da3fefe08b5389c2030fff1b12ea
infirit <infirit@gmail.com>
2015-12-18 15:50:09 +0100

    mechanism: Do not use eval to retrieve dhcpd handler class
    See #416

--- blueman/plugins/mechanism/Network.py
+++ blueman/plugins/mechanism/Network.py
@@ -10,6 +10,9 @@ import subprocess
 from gi.repository import GObject
 from blueman.main.NetConf import NetConf, DnsMasqHandler, DhcpdHandler, UdhcpdHandler
 
+DHCPDHANDLERS = {"DnsMasqHandler": DnsMasqHandler,
+                 "DhcpdHandler": DhcpdHandler,
+                 "UdhcpdHandle": UdhcpdHandler}
 
 class Network(MechanismPlugin):
     @dbus.service.method('org.blueman.Mechanism', in_signature="s", out_signature="s", sender_keyword="caller",
@@ -53,7 +56,7 @@ class Network(MechanismPlugin):
     def EnableNetwork(self, ip_address, netmask, dhcp_handler, caller):
         nc = NetConf.get_default()
         nc.set_ipv4(ip_address, netmask)
-        eval("nc.set_dhcp_handler(%s)" % dhcp_handler)
+        nc.set_dhcp_handler(DHCPDHANDLERS[dhcp_handler])
         nc.apply_settings()
 
     @dbus.service.method('org.blueman.Mechanism', in_signature="", out_signature="", sender_keyword="caller")
