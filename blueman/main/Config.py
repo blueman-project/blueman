1283b84d819fceec1d11df1d320b4dc466e38db9
infirit <infirit@gmail.com>
2016-01-15 23:23:41 +0100

    Config: Use super to handle object initialisation

--- blueman/main/Config.py
+++ blueman/main/Config.py
@@ -8,9 +8,10 @@ from gi.repository import Gio
 class Config(Gio.Settings):
     def __init__(self, schema_id, path=None):
         # Add backwards compat with pygobject < 3.11.2
-        new_init = Gio.Settings.__init__.__name__ == "new_init"
-        if new_init: Gio.Settings.__init__(self, schema_id=schema_id, path=path)
-        else: Gio.Settings.__init__(self, schema=schema_id, path=path)
+        if Gio.Settings.__init__.__name__ == "new_init":
+            super(Config, self).__init__(schema_id=schema_id, path=path)
+        else:
+            super(Config, self).__init__(schema=schema_id, path=path)
 
     def bind_to_widget(self, key, widget, prop, flags=Gio.SettingsBindFlags.DEFAULT):
         self.bind(key, widget, prop, flags)
