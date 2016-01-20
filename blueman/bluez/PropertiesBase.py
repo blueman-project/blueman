930b88adb6e6708e3440541b88cb672259c6fdd3
infirit <infirit@gmail.com>
2016-01-20 15:13:12 +0100

    PropertiesBase: Merge the two property changed functions
    _on_property_changed does nothing but emit a signal which we can just
    as well do from _on_properties_changed.

--- blueman/bluez/PropertiesBase.py
+++ blueman/bluez/PropertiesBase.py
@@ -27,14 +27,11 @@ class PropertiesBase(Base):
         self._handle_signal(self._on_properties_changed, 'PropertiesChanged', 'org.freedesktop.DBus.Properties',
                             path_keyword='path')
 
-    def _on_property_changed(self, key, value, path):
-        dprint(path, key, value)
-        self.emit('property-changed', key, value, path)
-
     def _on_properties_changed(self, interface_name, changed_properties, _invalidated_properties, path):
         if interface_name == self._interface_name:
             for name, value in changed_properties.items():
-                self._on_property_changed(name, value, path)
+                dprint(path, name, value)
+                self.emit('property-changed', name, value, path)
 
     @raise_dbus_error
     def get(self, name):
