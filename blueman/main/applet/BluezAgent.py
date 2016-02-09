85a5ca4dce10242cf1f8944c966130373cd4082b
infirit <infirit@gmail.com>
2016-02-09 11:30:27 +0100

    BluezAgent: Fix notification not closing in RequestConfirmation
    Agent.Cancel was called properly however we never set self.n in
    RequestConfirmation and hence fail to close the notification.

--- blueman/main/applet/BluezAgent.py
+++ blueman/main/applet/BluezAgent.py
@@ -215,9 +215,8 @@ class AdapterAgent(CommonAgent):
             notify_message += "\n" + _("Confirm value for authentication:") + " <b>%s</b>" % passkey
         actions = [["confirm", _("Confirm")], ["deny", _("Deny")]]
 
-        Notification("Bluetooth", notify_message, 0,
-                     actions, on_confirm_action,
-                     pixbuf=get_icon("blueman", 48), status_icon=self.status_icon)
+        self.n = Notification("Bluetooth", notify_message, 0, actions, on_confirm_action,
+                              pixbuf=get_icon("blueman", 48), status_icon=self.status_icon)
 
     @AgentMethod
     def RequestAuthorization(self, device, ok, err):
