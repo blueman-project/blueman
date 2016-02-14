blueman-project/blueman

commit 40cec2868681ce7ca4e869b90b65bf4f969a23d9

Do not shadow python builtins

blueman/gui/GenericList.py
@@ -49,20 +49,20 @@ class GenericList(Gtk.TreeView):
                 self.append_column(column)
 
     def selected(self):
-        (model, iter) = self.selection.get_selected()
+        (model, tree_iter) = self.selection.get_selected()
 
-        return iter
+        return tree_iter
 
-    def delete(self, id):
+    def delete(self, iterid):
         if type(id) == Gtk.TreeIter:
-            iter = id
+            tree_iter = iterid
         else:
-            iter = self.get_iter(id)
+            tree_iter = self.get_iter(id)
 
-        if iter is None:
+        if tree_iter is None:
             return False
-        if self.liststore.iter_is_valid(iter):
-            self.liststore.remove(iter)
+        if self.liststore.iter_is_valid(tree_iter):
+            self.liststore.remove(tree_iter)
             return True
         else:
             return False
@@ -104,34 +104,34 @@ class GenericList(Gtk.TreeView):
 
         return ret
 
-    def set(self, id, **cols):
-        if type(id) == Gtk.TreeIter:
-            iter = id
+    def set(self, iterid, **cols):
+        if type(iterid) == Gtk.TreeIter:
+            tree_iter = iterid
         else:
-            iter = self.get_iter(id)
+            tree_iter = self.get_iter(iterid)
 
-        if iter is not None:
+        if tree_iter is not None:
             for k, v in cols.items():
-                self.liststore.set(iter, self.ids[k], v)
+                self.liststore.set(tree_iter, self.ids[k], v)
             return True
         else:
             return False
 
-    def get(self, id, *items):
+    def get(self, iterid, *items):
         ret = {}
 
-        if id is not None:
-            if type(id) == Gtk.TreeIter:
-                iter = id
+        if iterid is not None:
+            if type(iterid) == Gtk.TreeIter:
+                tree_iter = iterid
             else:
-                iter = self.get_iter(id)
+                tree_iter = self.get_iter(iterid)
             if len(items) == 0:
                 for k, v in self.ids.items():
-                    ret[k] = self.liststore.get(iter, v)[0]
+                    ret[k] = self.liststore.get(tree_iter, v)[0]
             else:
                 for i in range(len(items)):
                     if items[i] in self.ids:
-                        ret[items[i]] = self.liststore.get(iter, self.ids[items[i]])[0]
+                        ret[items[i]] = self.liststore.get(tree_iter, self.ids[items[i]])[0]
         else:
             return False
 

apps/blueman-assistant
@@ -185,15 +185,15 @@ class Assistant:
             self.assistant.set_page_complete(pages[PAGE_PASSKEY], True)
             self.pair = pair
 
-    def on_device_selected(self, list, device, iter):
+    def on_device_selected(self, lst, device, tree_iter):
         if not device:
             self.assistant.set_page_complete(pages[PAGE_DEVLIST], False)
             self.Device = None
-            self.Adapter = list.Adapter
+            self.Adapter = lst.Adapter
         else:
             self.assistant.set_page_complete(pages[PAGE_DEVLIST], True)
             self.Device = device
-            self.Adapter = list.Adapter
+            self.Adapter = lst.Adapter
 
     def on_close(self, assistant):
         Gtk.main_quit()

apps/blueman-manager
@@ -164,7 +164,7 @@ class Blueman(Gtk.Window):
 
         dbus.SystemBus().watch_name_owner('org.bluez', on_bluez_name_owner_changed)
 
-    def on_adapter_changed(self, list, adapter):
+    def on_adapter_changed(self, lst, adapter):
         if adapter is not None:
             self.List.DisplayKnownDevices(autoselect=True)
 
@@ -176,7 +176,7 @@ class Blueman(Gtk.Window):
                 self.List.disconnect(s1)
                 self.List.disconnect(s2)
 
-        def on_progress(list, frac):
+        def on_progress(lst, frac):
             if abs(1.0 - frac) <= 0.00001:
                 if not prog.started():
                     prog.start()

apps/blueman-services
@@ -167,14 +167,14 @@ class BluemanServices(Gtk.Dialog):
                 inst._on_leave()
 
     def on_selection_changed(self, selection):
-        iter = self.List.selected()
+        tree_iter = self.List.selected()
         if self.List.get_cursor()[0]:
             # GtkTreePath returns row when used as string
             self.Config["services-last-item"] = int(str(self.List.get_cursor()[0]))
-        row = self.List.get(iter, "id")
-        id = row["id"]
+        row = self.List.get(tree_iter, "id")
+        rowid = row["id"]
 
-        self.set_page(id)
+        self.set_page(rowid)
 
 if __name__ == '__main__':
     services = BluemanServices()

blueman/gui/DeviceList.py
@@ -109,17 +109,17 @@ class DeviceList(GenericList):
         #self.clear()
         if len(self.liststore):
             for i in self.liststore:
-                iter = i.iter
-                device = self.get(iter, "device")["device"]
+                tree_iter = i.iter
+                device = self.get(tree_iter, "device")["device"]
             #device.Destroy()
         GenericList.destroy(self)
 
     def on_selection_changed(self, selection):
-        _model, iter = selection.get_selected()
-        if iter:
-            row = self.get(iter, "device")
+        _model, tree_iter = selection.get_selected()
+        if tree_iter:
+            row = self.get(tree_iter, "device")
             dev = row["device"]
-            self.emit("device-selected", dev, iter)
+            self.emit("device-selected", dev, tree_iter)
 
     def _on_property_changed(self, _adapter, key, value, _path):
         if key == "Discovering":
@@ -129,19 +129,19 @@ class DeviceList(GenericList):
         self.emit("adapter-property-changed", self.Adapter, (key, value))
 
     def _on_device_property_changed(self, _device, key, value, path):
-        iter = self.find_device_by_path(path)
+        tree_iter = self.find_device_by_path(path)
 
-        if iter is not None:
-            dev = self.get(iter, "device")["device"]
-            self.row_update_event(iter, key, value)
+        if tree_iter is not None:
+            dev = self.get(tree_iter, "device")["device"]
+            self.row_update_event(tree_iter, key, value)
 
-            self.emit("device-property-changed", dev, iter, (key, value))
+            self.emit("device-property-changed", dev, tree_iter, (key, value))
 
             if key == "Connected":
                 if value:
                     self.monitor_power_levels(dev)
                 else:
-                    r = Gtk.TreeRowReference.new(self.get_model(), self.props.model.get_path(iter))
+                    r = Gtk.TreeRowReference.new(self.get_model(), self.props.model.get_path(tree_iter))
                     self.level_setup_event(r, dev, None)
 
     def monitor_power_levels(self, device):
@@ -156,8 +156,8 @@ class DeviceList(GenericList):
                 self.monitored_devices.remove(props["Address"])
                 return False
 
-            iter = self.get_model().get_iter(row_ref.get_path())
-            device = self.get(iter, "device")["device"]
+            tree_iter = self.get_model().get_iter(row_ref.get_path())
+            device = self.get(tree_iter, "device")["device"]
             if not device['Connected']:
                 dprint("stopping monitor (not connected)")
                 cinfo.deinit()
@@ -172,7 +172,7 @@ class DeviceList(GenericList):
 
         if "Connected" in props and props["Connected"] and props["Address"] not in self.monitored_devices:
             dprint("starting monitor")
-            iter = self.find_device(device)
+            tree_iter = self.find_device(device)
 
             hci = os.path.basename(self.Adapter.get_object_path())
             try:
@@ -180,7 +180,7 @@ class DeviceList(GenericList):
             except Exception as e:
                 dprint("Failed to get power levels\n%s" % e)
             else:
-                r = Gtk.TreeRowReference.new(self.get_model(), self.get_model().get_path(iter))
+                r = Gtk.TreeRowReference.new(self.get_model(), self.get_model().get_path(tree_iter))
                 self.level_setup_event(r, device, cinfo)
                 GLib.timeout_add(1000, update, r, cinfo, props["Address"])
                 self.monitored_devices.append(props["Address"])
@@ -189,15 +189,15 @@ class DeviceList(GenericList):
 
     #called when power levels need updating
     #if cinfo is None then info icons need to be removed
-    def level_setup_event(self, iter, device, cinfo):
+    def level_setup_event(self, tree_iter, device, cinfo):
         pass
 
     #called when row needs to be initialized
-    def row_setup_event(self, iter, device):
+    def row_setup_event(self, tree_iter, device):
         pass
 
     #called when a property for a device changes
-    def row_update_event(self, iter, key, value):
+    def row_update_event(self, tree_iter, key, value):
         pass
 
     #called when device needs to be added to the list
@@ -205,24 +205,24 @@ class DeviceList(GenericList):
     def device_add_event(self, device):
         self.AppendDevice(device)
 
-    def device_remove_event(self, device, iter):
-        self.RemoveDevice(device, iter)
+    def device_remove_event(self, device, tree_iter):
+        self.RemoveDevice(device, tree_iter)
 
     #########################
 
     def _on_device_created(self, _adapter, path):
-        iter = self.find_device_by_path(path)
-        if iter is None:
+        tree_iter = self.find_device_by_path(path)
+        if tree_iter is None:
             dev = Bluez.Device(path)
             self.device_add_event(dev)
 
     def _on_device_removed(self, _manager, path):
-        iter = self.find_device_by_path(path)
-        if iter:
-            row = self.get(iter, "device")
+        tree_iter = self.find_device_by_path(path)
+        if tree_iter:
+            row = self.get(tree_iter, "device")
             dev = row["device"]
 
-            self.device_remove_event(dev, iter)
+            self.device_remove_event(dev, tree_iter)
 
     def SetAdapter(self, adapter=None):
         self.clear()
@@ -268,24 +268,24 @@ class DeviceList(GenericList):
         return True
 
     def add_device(self, device, append=True):
-        iter = self.find_device(device)
+        tree_iter = self.find_device(device)
         #device belongs to another adapter
         if not device.get_object_path().startswith(self.Adapter.get_object_path()):
             return
 
-        if iter is None:
+        if tree_iter is None:
             dprint("adding new device")
             if append:
-                iter = self.liststore.append()
+                tree_iter = self.liststore.append()
             else:
-                iter = self.liststore.prepend()
+                tree_iter = self.liststore.prepend()
 
-            self.set(iter, device=device)
-            self.row_setup_event(iter, device)
+            self.set(tree_iter, device=device)
+            self.row_setup_event(tree_iter, device)
 
             props = device.get_properties()
             try:
-                self.set(iter, dbus_path=device.get_object_path())
+                self.set(tree_iter, dbus_path=device.get_object_path())
             except:
                 pass
 
@@ -294,15 +294,15 @@ class DeviceList(GenericList):
                 self.monitor_power_levels(device)
 
         else:
-            row = self.get(iter, "device")
+            row = self.get(tree_iter, "device")
             existing_dev = row["device"]
 
             props = existing_dev.get_properties()
             props_new = device.get_properties()
 
             existing_dev.disconnect_signal('property-changed')
-            self.set(iter, device=device, dbus_path=device.get_object_path())
-            self.row_setup_event(iter, device)
+            self.set(tree_iter, device=device, dbus_path=device.get_object_path())
+            self.row_setup_event(tree_iter, device)
             
             device.connect_signal('property-changed', self._on_device_property_changed)
 
@@ -348,15 +348,15 @@ class DeviceList(GenericList):
     def AppendDevice(self, device):
         self.add_device(device, True)
 
-    def RemoveDevice(self, device, iter=None):
+    def RemoveDevice(self, device, tree_iter=None):
         dprint(device)
-        if iter is None:
-            iter = self.find_device(device)
+        if tree_iter is None:
+            tree_iter = self.find_device(device)
 
-        if self.compare(self.selected(), iter):
+        if self.compare(self.selected(), tree_iter):
             self.emit("device-selected", None, None)
 
-        self.delete(iter)
+        self.delete(tree_iter)
 
     def GetSelectedDevice(self):
         selected = self.selected()
@@ -368,9 +368,9 @@ class DeviceList(GenericList):
     def clear(self):
         if len(self.liststore):
             for i in self.liststore:
-                iter = i.iter
-                device = self.get(iter, "device")["device"]
-                self.RemoveDevice(device, iter)
+                tree_iter = i.iter
+                device = self.get(tree_iter, "device")["device"]
+                self.RemoveDevice(device, tree_iter)
             self.liststore.clear()
             self.emit("device-selected", None, None)
 
@@ -387,8 +387,8 @@ class DeviceList(GenericList):
             row = self.address_to_row[address]
             if row.valid():
                 path = row.get_path()
-                iter = self.get_model().get_iter(path)
-                return iter
+                tree_iter = self.get_model().get_iter(path)
+                return tree_iter
             else:
                 del self.address_to_row[address]
                 return None
@@ -401,38 +401,38 @@ class DeviceList(GenericList):
             row = self.path_to_row[path]
             if row.valid():
                 path = row.get_path()
-                iter = self.get_model().get_iter(path)
-                return iter
+                tree_iter = self.get_model().get_iter(path)
+                return tree_iter
             else:
                 del self.path_to_row[path]
                 return None
         except KeyError:
             return None
 
-    def do_cache(self, iter, kwargs):
+    def do_cache(self, tree_iter, kwargs):
         if "device" in kwargs:
             if kwargs["device"]:
                 self.address_to_row[kwargs["device"]['Address']] = Gtk.TreeRowReference.new(self.get_model(),
-                                                                                         self.get_model().get_path(iter))
+                                                                                            self.get_model().get_path(tree_iter))
                 dprint("Caching new device %s" % kwargs["device"]['Address'])
 
         if "dbus_path" in kwargs:
             if kwargs["dbus_path"] is not None:
                 self.path_to_row[kwargs["dbus_path"]] = Gtk.TreeRowReference.new(self.get_model(),
-                                                                                 self.get_model().get_path(iter))
+                                                                                 self.get_model().get_path(tree_iter))
             else:
-                existing = self.get(iter, "dbus_path")["dbus_path"]
+                existing = self.get(tree_iter, "dbus_path")["dbus_path"]
                 if existing is not None:
                     del self.path_to_row[existing]
 
     def append(self, **columns):
-        iter = GenericList.append(self, **columns)
-        self.do_cache(iter, columns)
+        tree_iter = GenericList.append(self, **columns)
+        self.do_cache(tree_iter, columns)
 
     def prepend(self, **columns):
-        iter = GenericList.prepend(self, **columns)
-        self.do_cache(iter, columns)
+        tree_iter = GenericList.prepend(self, **columns)
+        self.do_cache(tree_iter, columns)
 
-    def set(self, iter, **kwargs):
-        self.do_cache(iter, kwargs)
-        GenericList.set(self, iter, **kwargs)
+    def set(self, tree_iter, **kwargs):
+        self.do_cache(tree_iter, kwargs)
+        GenericList.set(self, tree_iter, **kwargs)

blueman/gui/DeviceSelectorDialog.py
@@ -49,7 +49,7 @@ class DeviceSelectorDialog(Gtk.Dialog):
     def on_adapter_changed(self, devlist, adapter):
         self.selection = None
 
-    def on_device_selected(self, devlist, device, iter):
+    def on_device_selected(self, devlist, device, tree_iter):
         self.selection = (devlist.Adapter.get_object_path(), device)
 
     def GetSelection(self):

blueman/gui/DeviceSelectorList.py
@@ -40,30 +40,30 @@ class DeviceSelectorList(DeviceList):
         super(DeviceSelectorList, self).__init__(adapter, data)
         self.props.headers_visible = False
 
-    def row_setup_event(self, iter, device):
-        self.row_update_event(iter, "Trusted", device['Trusted'])
-        self.row_update_event(iter, "Paired", device['Paired'])
-        self.row_update_event(iter, "Alias", device['Alias'])
-        self.row_update_event(iter, "Icon", device['Icon'])
+    def row_setup_event(self, tree_iter, device):
+        self.row_update_event(tree_iter, "Trusted", device['Trusted'])
+        self.row_update_event(tree_iter, "Paired", device['Paired'])
+        self.row_update_event(tree_iter, "Alias", device['Alias'])
+        self.row_update_event(tree_iter, "Icon", device['Icon'])
 
     def device_add_event(self, device):
         self.AppendDevice(device)
 
-    def row_update_event(self, iter, key, value):
+    def row_update_event(self, tree_iter, key, value):
         if key == "Trusted":
             if value:
-                self.set(iter, trusted_icon=get_icon("blueman-trust", 16))
+                self.set(tree_iter, trusted_icon=get_icon("blueman-trust", 16))
             else:
-                self.set(iter, trusted_icon=None)
+                self.set(tree_iter, trusted_icon=None)
 
         elif key == "Paired":
             if value:
-                self.set(iter, bonded_icon=get_icon("dialog-password", 16))
+                self.set(tree_iter, bonded_icon=get_icon("dialog-password", 16))
             else:
-                self.set(iter, bonded_icon=None)
+                self.set(tree_iter, bonded_icon=None)
 
         elif key == "Alias":
-            self.set(iter, caption=cgi.escape(value))
+            self.set(tree_iter, caption=cgi.escape(value))
 
         elif key == "Icon":
-            self.set(iter, device_pb=get_icon(value, 16))
+            self.set(tree_iter, device_pb=get_icon(value, 16))

blueman/gui/DeviceSelectorWidget.py
@@ -95,9 +95,9 @@ class DeviceSelectorWidget(Gtk.Box):
 
     def on_adapter_selected(self, cb_adapters):
         dprint("selected")
-        iter = cb_adapters.get_active_iter()
-        if iter:
-            adapter_path = cb_adapters.get_model().get_value(iter, 1)
+        tree_iter = cb_adapters.get_active_iter()
+        if tree_iter:
+            adapter_path = cb_adapters.get_model().get_value(tree_iter, 1)
             if self.List.Adapter:
                 if self.List.Adapter.get_object_path() != adapter_path:
                     self.List.SetAdapter(os.path.basename(adapter_path))
@@ -125,6 +125,6 @@ class DeviceSelectorWidget(Gtk.Box):
             self.List.props.sensitive = True
             self.cb_adapters.props.visible = True
             for adapter in adapters:
-                iter = self.cb_adapters.get_model().append([adapter.get_name(), adapter.get_object_path()])
+                tree_iter = self.cb_adapters.get_model().append([adapter.get_name(), adapter.get_object_path()])
                 if adapter.get_object_path() == self.List.Adapter.get_object_path():
-                    self.cb_adapters.set_active_iter(iter)
+                    self.cb_adapters.set_active_iter(tree_iter)

commit 2a295650e3e6d55edb718b96b44bd3b74339516f

GenericList: Correct typo from cleanup
The mistake was introduced in 40cec2868681ce7ca4e869b90b65bf4f969a23d9

blueman/gui/GenericList.py
@@ -54,10 +54,10 @@ class GenericList(Gtk.TreeView):
         return tree_iter
 
     def delete(self, iterid):
-        if type(id) == Gtk.TreeIter:
+        if type(iterid) == Gtk.TreeIter:
             tree_iter = iterid
         else:
-            tree_iter = self.get_iter(id)
+            tree_iter = self.get_iter(iterid)
 
         if tree_iter is None:
             return False