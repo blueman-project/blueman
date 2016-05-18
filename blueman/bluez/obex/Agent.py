533a6c5b8cf13ed3a241a82e63c39adbb449e709
Sander Sweers <infirit@gmail.com>
2016-05-18 23:07:45 +0200

    Port ObexAgent to GDBus

--- blueman/bluez/obex/Agent.py
+++ blueman/bluez/obex/Agent.py
@@ -4,58 +4,42 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import dbus
-import dbus.service
-from gi.repository import GObject
-from gi.types import GObjectMeta
-from inspect import isclass
-from blueman.Functions import dprint
-from blueman.bluez.obex.Error import Error
+from gi.repository import Gio, GLib
 
+introspection_xml = \
+'''
+<node name='/org/blueman/obex_agent'>
+  <interface name='org.bluez.obex.Agent1'>
+    <method name='Release'/>
+    <method name='Cancel'/>
+    <method name ='AuthorizePush'>
+      <arg type='o' name='transfer' direction='in'/>
+      <arg type='s' name='path' direction='out'/>
+    </method>
+  </interface>
+</node>
+'''
 
-class _GDbusObjectType(dbus.service.InterfaceType, GObjectMeta):
-    pass
+class Agent(object):
+    __bus = Gio.bus_get_sync(Gio.BusType.SESSION)
 
-_GDBusObject = _GDbusObjectType(str('_GDBusObject'), (dbus.service.Object, GObject.GObject), {})
+    def __init__(self, agent_path, handle_method_call):
+        node_info = Gio.DBusNodeInfo.new_for_xml(introspection_xml)
 
+        regid = self.__bus.register_object(
+            agent_path,
+            node_info.interfaces[0],
+            handle_method_call,
+            None,
+            None)
 
-# noinspection PyPep8Naming
-class Agent(_GDBusObject, dbus.service.Object, GObject.GObject):
-    __gsignals__ = {
-        str('release'): (GObject.SignalFlags.NO_HOOKS, None, ()),
-        str('authorize'): (GObject.SignalFlags.NO_HOOKS, None, (GObject.TYPE_PYOBJECT,)),
-        str('cancel'): (GObject.SignalFlags.NO_HOOKS, None, ()),
-    }
-
-    def __init__(self, agent_path):
-        self._agent_path = agent_path
-        dbus.service.Object.__init__(self, dbus.SessionBus(), agent_path)
-        GObject.GObject.__init__(self)
-        self._reply_handler = None
-        self._error_handler = None
-
-    @dbus.service.method('org.bluez.obex.Agent1')
-    def Release(self):
-        dprint(self._agent_path)
-        self.emit('release')
-
-    @dbus.service.method('org.bluez.obex.Agent1', async_callbacks=('reply_handler', 'error_handler'))
-    def AuthorizePush(self, transfer_path, reply_handler, error_handler):
-        dprint(self._agent_path, transfer_path)
-        self._reply_handler = reply_handler
-        self._error_handler = error_handler
-        self.emit('authorize', transfer_path)
-
-    @dbus.service.method('org.bluez.obex.Agent1')
-    def Cancel(self):
-        dprint(self._agent_path)
-        self.emit('cancel')
-
-    def reply(self, reply):
-        dprint(self._agent_path, reply)
-        if isclass(reply) and issubclass(reply, Error):
-            self._error_handler(dbus.DBusException(name=('org.bluez.obex.Error.%s' % reply.__name__)))
+        if regid:
+            self.__regid = regid
         else:
-            self._reply_handler(reply)
-        self._reply_handler = None
-        self._error_handler = None
+            raise GLib.Error('Failed to register object with path: %s', agent_path)
+
+    def __del__(self):
+        self._unregister_object()
+
+    def _unregister_object(self):
+        self.__bus.unregister_object(self.__regid)
--- blueman/plugins/applet/TransferService.py
+++ blueman/plugins/applet/TransferService.py
@@ -16,52 +16,65 @@ from blueman.gui.Notification import Notification
 from blueman.plugins.AppletPlugin import AppletPlugin
 from blueman.main.Config import Config
 
-class _Agent:
+class Agent(obex.Agent):
+    __agent_path = '/org/bluez/obex/agent/blueman'
+
     def __init__(self, applet):
+        super(Agent, self).__init__(self.__agent_path, self._handle_method_call)
+
         self._applet = applet
         self._config = Config("org.blueman.transfer")
-
-        self._agent_path = '/org/blueman/obex_agent'
-
-        self._agent = obex.Agent(self._agent_path)
-        self._agent.connect('release', self._on_release)
-        self._agent.connect('authorize', self._on_authorize)
-        self._agent.connect('cancel', self._on_cancel)
 
         self._allowed_devices = []
         self._notification = None
         self._pending_transfer = None
         self.transfers = {}
 
+    def _handle_method_call(self, connection, sender, agent_path, interface_name, method_name, parameters, invocation):
+        if method_name == 'Release':
+            dprint(agent_path)
+            self._on_release()
+        elif method_name == 'AuthorizePush':
+            dprint(agent_path)
+            self._on_authorize(parameters, invocation)
+        elif method_name == 'Cancel':
+            dprint(agent_path)
+            self._on_cancel(parameters, invocation)
+
     def register(self):
-        obex.AgentManager().register_agent(self._agent_path)
+        obex.AgentManager().register_agent(self.__agent_path)
 
     def unregister(self):
-        obex.AgentManager().unregister_agent(self._agent_path)
+        obex.AgentManager().unregister_agent(self.__agent_path)
 
-    def _on_release(self, _agent):
-        raise Exception(self._agent_path + " was released unexpectedly")
+    def _on_release(self):
+        raise Exception(self.__agent_path + " was released unexpectedly")
 
-    def _on_action(self, _notification, action):
-        dprint(action)
+    def _on_authorize(self, parameters, invocation):
+        def on_action(_notification, action):
+            dprint(action)
 
-        if action == "accept":
-            self.transfers[self._pending_transfer['transfer_path']] = {
-                'path': self._pending_transfer['root'] + '/' + os.path.basename(self._pending_transfer['filename']),
-                'size': self._pending_transfer['size'],
-                'name': self._pending_transfer['name']
-            }
-            self._agent.reply(self.transfers[self._pending_transfer['transfer_path']]['path'])
-            self._allowed_devices.append(self._pending_transfer['address'])
-            GLib.timeout_add(60000, self._allowed_devices.remove, self._pending_transfer['address'])
-        else:
-            self._agent.reply(obex.Error.Rejected)
+            if action == "accept":
+                self.transfers[self._pending_transfer['transfer_path']] = {
+                    'path': self._pending_transfer['root'] + '/' + os.path.basename(self._pending_transfer['filename']),
+                    'size': self._pending_transfer['size'],
+                    'name': self._pending_transfer['name']
+                }
 
-    def _on_authorize(self, _agent, transfer_path):
+                param = GLib.Variant('(s)', (self.transfers[self._pending_transfer['transfer_path']]['path'],))
+                invocation.return_value(param)
+
+                self._allowed_devices.append(self._pending_transfer['address'])
+                GLib.timeout_add(60000, self._allowed_devices.remove, self._pending_transfer['address'])
+            else:
+                invocation.return_dbus_error('org.bluez.obex.Error.Rejected', 'Rejected')
+
+        transfer_path = parameters.unpack()[0]
+
         transfer = obex.Transfer(transfer_path)
         session = obex.Session(transfer.session)
-        root = session.root
-        address = session.address
+        root = session.root[0]
+        address = session.address[0]
         filename = transfer.name
         size = transfer.size
 
@@ -87,7 +100,7 @@ class _Agent:
             self._notification = Notification(_("Incoming file over Bluetooth"),
                 _("Incoming file %(0)s from %(1)s") % {"0": "<b>" + filename + "</b>",
                                                        "1": "<b>" + name + "</b>"},
-                30000, [["accept", _("Accept"), "help-about"], ["reject", _("Reject"), "help-about"]], self._on_action,
+                30000, [["accept", _("Accept"), "help-about"], ["reject", _("Reject"), "help-about"]], on_action,
                 pixbuf=get_icon("blueman", 48), status_icon=status_icon)
         # Device is trusted or was already allowed, larger file -> display a notification, but auto-accept
         elif size > 350000:
@@ -95,14 +108,15 @@ class _Agent:
                 _("Receiving file %(0)s from %(1)s") % {"0": "<b>" + filename + "</b>",
                                                         "1": "<b>" + name + "</b>"},
                 pixbuf=get_icon("blueman", 48), status_icon=status_icon)
-            self._on_action(self._notification, 'accept')
+            on_action(self._notification, 'accept')
         # Device is trusted or was already allowed. very small file -> auto-accept and transfer silently
         else:
             self._notification = None
-            self._on_action(self._notification, "accept")
+            on_action(self._notification, "accept")
 
-    def _on_cancel(self, agent):
+    def _on_cancel(self, parameters, invocation):
         self._notification.close()
+        invocation.return_dbus_error('org.bluez.obex.Error.Canceled', 'Canceled')
         agent.reply(obex.Error.Canceled)
 
 
@@ -157,7 +171,7 @@ class TransferService(AppletPlugin):
 
     def _register_agent(self):
         if not self.__class__._agent:
-            self.__class__._agent = _Agent(self._applet)
+            self.__class__._agent = Agent(self._applet)
         self._agent.register()
 
     @classmethod
