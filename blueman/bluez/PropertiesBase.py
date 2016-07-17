a4839afe4a75babbe9a9245de486b5dbcace904b
Sander Sweers <infirit@gmail.com>
2016-07-17 11:29:09 +0200

    PropertiesBase: Add fallback when cached properties are not available
    We run into problem when bluez is restarted and GDBus has not had the time
    to populate the cached properties.

--- blueman/bluez/PropertiesBase.py
+++ blueman/bluez/PropertiesBase.py
@@ -44,10 +44,18 @@ class PropertiesBase(Base):
     def get(self, name):
         prop = self._dbus_proxy.get_cached_property(name)
 
-        if prop is not None:
-            return prop.unpack()
-        elif not prop and name in self.__fallback:
+        if prop is None and name in self.__fallback:
             return self.__fallback[name]
+        elif prop is None:
+            # Fallback when cached property is not available
+            param = GLib.Variant('(ss)', (self._interface_name, name))
+            try:
+                prop = self._call('Get', param, True)
+                return prop[0]
+            except GLib.GError:
+                raise BluezDBusException("No such property '%s'" % name)
+        elif prop is not None:
+            return prop.unpack()
         else:
             raise BluezDBusException("No such property '%s'" % name)
 
@@ -57,10 +65,13 @@ class PropertiesBase(Base):
         self._call('Set', param, True)
 
     def get_properties(self):
-        prop_names = self._dbus_proxy.get_cached_property_names()
+        param = GLib.Variant('(s)', (self._interface_name,))
+        res = self._call('GetAll', param, True)
+        prop_names = res[0].keys()
+
         result = {}
         for name in prop_names:
-            result[name] = self._dbus_proxy.get_cached_property(name).unpack()
+            result[name] = self.get(name)
 
         if result:
             for k, v in self.__fallback.items():
