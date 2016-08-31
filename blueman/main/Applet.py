8dd23a740720407195530ccfe1d9f0095e28ef16
Sander Sweers <infirit@gmail.com>
2016-08-31 14:05:36 +0200

    Move application classes out of the scripts installed in /usr/bin
    This allows us to easily import them in other places and use them.

---
+++ blueman/main/Applet.py
@@ -0,0 +1,93 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from blueman.Functions import *
+import blueman.bluez as Bluez
+import blueman.plugins.applet
+from blueman.main.PluginManager import PersistentPluginManager
+from blueman.main.DbusService import DbusService
+from blueman.plugins.AppletPlugin import AppletPlugin
+
+import gi
+gi.require_version("Gtk", "3.0")
+gi.require_version('Notify', '0.7')
+from gi.repository import Notify
+try: import __builtin__ as builtins
+except ImportError: import builtins
+
+class BluemanApplet(object):
+    def __init__(self):
+        setup_icon_path()
+        if not Notify.init("Blueman"):
+            dprint("Error: Failed to initialize libnotify")
+
+        check_single_instance("blueman-applet")
+
+        self._signals = []
+
+        self.plugin_run_state_changed = False
+
+        self.Manager = Bluez.Manager()
+        self._signals.append(self.Manager.connect_signal('adapter-added', self.on_adapter_added))
+        self._signals.append(self.Manager.connect_signal('adapter-removed', self.on_adapter_removed))
+        self._signals.append(self.Manager.connect_signal('device-created', self.on_device_created))
+        self._signals.append(self.Manager.connect_signal('device-removed', self.on_device_removed))
+
+        self.DbusSvc = DbusService("org.blueman.Applet", "/")
+
+        self.Plugins = PersistentPluginManager(AppletPlugin, blueman.plugins.applet, self)
+        self.Plugins.Load()
+
+        self.Plugins.Run("on_plugins_loaded")
+
+        self.Manager.watch_name_owner(self._on_dbus_name_appeared, self._on_dbus_name_vanished)
+
+        self._any_adapter = Bluez.AnyAdapter()
+        self._any_adapter.connect_signal('property-changed', self._on_adapter_property_changed)
+
+        self._any_device = Bluez.AnyDevice()
+        self._any_device.connect_signal('property-changed', self._on_device_property_changed)
+
+        Gtk.main()
+
+    def _on_dbus_name_appeared(self, _connection, name, owner):
+        dprint(name, owner)
+        self.manager_state = True
+        self.plugin_run_state_changed = True
+        self.Plugins.Run("on_manager_state_changed", self.manager_state)
+
+    def _on_dbus_name_vanished(self, _connection, name):
+        dprint(name)
+        self.manager_state = False
+        self.plugin_run_state_changed = True
+        self.Plugins.Run("on_manager_state_changed", self.manager_state)
+
+    def _on_adapter_property_changed(self, _adapter, key, value, path):
+        self.Plugins.Run("on_adapter_property_changed", path, key, value)
+
+    def _on_device_property_changed(self, _device, key, value, path):
+        self.Plugins.Run("on_device_property_changed", path, key, value)
+
+    def on_adapter_added(self, _manager, path):
+        dprint("Adapter added ", path)
+
+        def on_activate():
+            dprint("Adapter activated")
+            self.Plugins.Run("on_adapter_added", path)
+
+        adapter = Bluez.Adapter(path)
+        wait_for_adapter(adapter, on_activate)
+
+    def on_adapter_removed(self, _manager, path):
+        dprint("Adapter removed ", path)
+        self.Plugins.Run("on_adapter_removed", path)
+
+    def on_device_created(self, _manager, path):
+        dprint("Device created ", path)
+        self.Plugins.Run("on_device_created", path)
+
+    def on_device_removed(self, _manager, path):
+        dprint("Device removed ", path)
+        self.Plugins.Run("on_device_removed", path)
---
+++ blueman/main/Manager.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python@PYTHON_VERSION@
+# coding=utf-8
+
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import os.path
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+from locale import bind_textdomain_codeset
+
+from blueman.Functions import *
+from blueman.gui.manager.ManagerDeviceList import ManagerDeviceList
+from blueman.gui.manager.ManagerToolbar import ManagerToolbar
+from blueman.gui.manager.ManagerMenu import ManagerMenu
+from blueman.gui.manager.ManagerStats import ManagerStats
+from blueman.gui.manager.ManagerProgressbar import ManagerProgressbar
+from blueman.main.Config import Config
+import blueman.bluez as bluez
+
+from blueman.gui.MessageArea import MessageArea
+from blueman.gui.Notification import Notification
+
+from blueman.main.PluginManager import PluginManager
+import blueman.plugins.manager
+from blueman.plugins.ManagerPlugin import ManagerPlugin
+
+class Blueman(Gtk.Window):
+    def __init__(self):
+        super(Blueman, self).__init__(title=_("Bluetooth Devices"))
+
+        self._applet_sig = None
+
+        self.Config = Config("org.blueman.general")
+
+        self.Builder = Gtk.Builder()
+        self.Builder.set_translation_domain("blueman")
+        bind_textdomain_codeset("blueman", "UTF-8")
+        self.Builder.add_from_file(UI_PATH + "/manager-main.ui")
+
+        grid = self.Builder.get_object("grid")
+        self.add(grid)
+        self.set_name("BluemanManager")
+
+        self.Plugins = PluginManager(ManagerPlugin, blueman.plugins.manager, None)
+        self.Plugins.Load()
+
+        area = MessageArea()
+        grid.attach(area, 0, 3, 1, 1)
+
+        # Add margin for resize grip or it will overlap
+        if self.get_has_resize_grip():
+            statusbar = self.Builder.get_object("statusbar")
+            margin_right = statusbar.get_margin_right()
+            statusbar.set_margin_right(margin_right + 10)
+
+        def do_present(time):
+            if self.props.visible:
+                self.present_with_time(time)
+
+        check_single_instance("blueman-manager", do_present)
+
+        def on_window_delete(window, event):
+            w, h = self.get_size()
+            x, y = self.get_position()
+            self.Config["window-properties"] = [w, h, x, y]
+            Gtk.main_quit()
+
+        def bt_status_changed(status):
+            if not status:
+                self.hide()
+                check_bluetooth_status(_("Bluetooth needs to be turned on for the device manager to function"),
+                                       lambda: Gtk.main_quit())
+            else:
+                self.show()
+
+        def on_applet_signal(_proxy, _sender, signal_name, params):
+            if signal_name == 'BluetoothStatusChanged':
+                status = params.unpack()
+                bt_status_changed(status)
+
+        def on_dbus_name_vanished(_connection, name):
+            dprint(name)
+            self.Applet.disconnect(self._applet_sig)
+            self._applet_sig = None
+
+            self.hide()
+            d = Gtk.MessageDialog(self, type=Gtk.MessageType.ERROR, buttons=Gtk.ButtonsType.CLOSE)
+            d.props.icon_name = "blueman"
+            d.props.text = _("Connection to BlueZ failed")
+
+            d.props.secondary_text = _(
+                "Bluez daemon is not running, blueman-manager cannot continue.\nThis probably means that there were no Bluetooth adapters detected or Bluetooth daemon was not started.")
+
+            d.run()
+            d.destroy()
+            try:
+                exit(1)
+            except:
+                Gtk.main_quit()
+
+        def on_dbus_name_appeared(_connection, name, owner):
+            dprint(name, owner)
+            setup_icon_path()
+
+            try:
+                self.Applet = AppletService()
+            except:
+                print("Blueman applet needs to be running")
+                exit()
+            try:
+                if not self.Applet.GetBluetoothStatus():
+                    bt_status_changed(False)
+            except:
+                pass
+
+            self._applet_sig = self.Applet.connect('g-signal', on_applet_signal)
+
+            self.connect("delete-event", on_window_delete)
+            self.props.icon_name = "blueman"
+
+            w, h, x, y = self.Config["window-properties"]
+            if w and h: self.resize(w, h)
+            if x and y: self.move(x, y)
+
+            sw = self.Builder.get_object("scrollview")
+            # Disable overlay scrolling
+            if Gtk.get_minor_version() >= 16:
+                sw.props.overlay_scrolling = False
+
+            self.List = ManagerDeviceList(adapter=self.Config["last-adapter"], inst=self)
+
+            self.List.show()
+            sw.add(self.List)
+
+            self.Toolbar = ManagerToolbar(self)
+            self.Menu = ManagerMenu(self)
+            self.Stats = ManagerStats(self)
+
+            if self.List.IsValidAdapter():
+                self.List.DisplayKnownDevices(autoselect=True)
+
+            self.List.connect("adapter-changed", self.on_adapter_changed)
+
+            toolbar = self.Builder.get_object("toolbar")
+            statusbar = self.Builder.get_object("statusbar")
+
+            self.Config.bind_to_widget("show-toolbar", toolbar, "visible")
+            self.Config.bind_to_widget("show-statusbar", statusbar, "visible")
+
+            self.show()
+
+        bluez.Manager.watch_name_owner(on_dbus_name_appeared, on_dbus_name_vanished)
+
+    def on_adapter_changed(self, lst, adapter):
+        if adapter is not None:
+            self.List.DisplayKnownDevices(autoselect=True)
+
+    def inquiry(self):
+        def prop_changed(List, adapter, key_value):
+            key, value = key_value
+            if key == "Discovering" and not value:
+                prog.finalize()
+                self.List.disconnect(s1)
+                self.List.disconnect(s2)
+
+        def on_progress(lst, frac):
+            if abs(1.0 - frac) <= 0.00001:
+                if not prog.started():
+                    prog.start()
+            else:
+                prog.fraction(frac)
+
+        prog = ManagerProgressbar(self, text=_("Searching"))
+        prog.connect("cancelled", lambda x: self.List.StopDiscovery())
+        try:
+            self.List.DiscoverDevices()
+        except Exception as e:
+            prog.finalize()
+            MessageArea.show_message(*e_(e))
+
+        s1 = self.List.connect("discovery-progress", on_progress)
+        s2 = self.List.connect("adapter-property-changed", prop_changed)
+
+    def setup(self, device):
+        command = "blueman-assistant --device=%s" % device['Address']
+        launch(command, None, False, "blueman", _("Bluetooth Assistant"))
+
+    def bond(self, device):
+        def error_handler(e):
+            dprint(e)
+            message = 'Pairing failed for:\n%s (%s)' % (device['Alias'], device['Address'])
+            Notification('Bluetooth', message, icon_name="blueman").show()
+
+        device.pair(error_handler=error_handler)
+
+    def adapter_properties(self):
+        adapter = self.List.Adapter
+        name = os.path.basename(adapter.get_object_path())
+        launch("blueman-adapters", None, False, "blueman", _("Adapter Preferences"))
+
+    def toggle_trust(self, device):
+        props = device.get_properties()
+        device['Trusted'] = not device['Trusted']
+
+    def send(self, device, File=None):
+        adapter = self.List.Adapter
+        props = adapter.get_properties()
+
+        command = "blueman-sendto --source=%s --device=%s" % (props["Address"], device['Address'])
+        launch(command, None, False, "blueman", _("File Sender"))
+
+    def remove(self, device):
+        self.List.Adapter.remove_device(device)
+
+    def disconnect(self, device, **kwargs):
+        applet = AppletService()
+        applet.DisconnectDevice(str('(s)'), device.get_object_path(), **kwargs)
