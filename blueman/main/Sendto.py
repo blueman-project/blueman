0dee1bd902f8aa781f983c1635d9ce988a77aa06
Sander Sweers <infirit@gmail.com>
2016-09-09 23:45:32 +0200

    SendTo: Add forgotten future imports

--- blueman/main/Sendto.py
+++ blueman/main/Sendto.py
@@ -1,3 +1,8 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
 import urllib
 import time
 from locale import bind_textdomain_codeset
