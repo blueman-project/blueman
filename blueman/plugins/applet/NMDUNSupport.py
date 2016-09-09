92f2d02de044a31ee49863e1cbead712c1632228
Sander Sweers <infirit@gmail.com>
2016-09-10 00:08:22 +0200

    NMDUnSupport: Port to GDBus

--- blueman/plugins/applet/NMDUNSupport.py
+++ blueman/plugins/applet/NMDUNSupport.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 from blueman.plugins.AppletPlugin import AppletPlugin
-import dbus
-from gi.repository import GLib
+from gi.repository import Gio, GLib
 from blueman.gui.Notification import Notification
 from blueman.Sdp import DIALUP_NET_SVCLASS_ID
 from blueman.Functions import get_icon, composite_icon
@@ -18,17 +17,19 @@ class ConnectionHandler:
         self.rfcomm_dev = None
         self.timeout = None
 
-        self.parent.bus.add_signal_receiver(self.on_interfaces_added, "InterfacesAdded",
-                                            "org.freedesktop.DBus.ObjectManager")
+        self.signal_sub = self.parent._bus.signal_subscribe("org.freedesktop.ModemManager1",
+                                                            "org.freedesktop.DBus.ObjectManager",
+                                                            "InterfacesAdded",
+                                                            "/org/freedesktop/ModemManager1",
+                                                            None,
+                                                            Gio.DBusSignalFlags.NONE,
+                                                            self.on_interfaces_added)
 
         # for some reason these handlers take a reference and don't give it back
         # so i have to workaround :(
         w = weakref.ref(self)
         service.connect(reply_handler=lambda *args: w() and w().on_connect_reply(*args),
                         error_handler=lambda *args: w() and w().on_connect_error(*args))
-
-    def __del__(self):
-        logging.info("deleting")
 
     def on_connect_reply(self, rfcomm):
         self.rfcomm_dev = rfcomm
@@ -41,22 +42,20 @@ class ConnectionHandler:
     def cleanup(self):
         if self.timeout:
             GLib.source_remove(self.timeout)
-        self.parent.bus.remove_signal_receiver(self.on_interfaces_added, "InterfacesAdded",
-                                               "org.freedesktop.DBus.ObjectManager")
 
-        del self.service
+        self.parent._bus.signal_unsubscribe(self.signal_sub)
+        self.signal_sub = None
+        self.service = None
 
     def on_mm_device_added(self, path, name="org.freedesktop.ModemManager"):
         logging.info(path)
         interface = "%s.Modem" % name
-        props = self.parent.bus.call_blocking(name, path, "org.freedesktop.DBus.Properties", "GetAll", "s", [interface])
+        res = self.parent._bus.call_sync(name, path, 'org.freedesktop.DBus.Properties', "GetAll",
+                                         GLib.Variant("(s)", (interface,)),
+                                         None, Gio.DBusCallFlags.NONE, GLib.MAXINT, None)
+        props = res.unpack()[0]
 
-        try:
-            drivers = props["Drivers"]
-        except KeyError:
-            drivers = [props["Driver"]]
-
-        if self.rfcomm_dev and "bluetooth" in drivers and props["Device"] in self.rfcomm_dev:
+        if self.rfcomm_dev and "bluetooth" in props["Drivers"] and self.rfcomm_dev.split('/')[-1] in props["Device"]:
             logging.info("It's our bluetooth modem!")
 
             modem = get_icon("modem", 24)
@@ -72,13 +71,14 @@ class ConnectionHandler:
             self.reply(self.rfcomm_dev)
             self.cleanup()
 
-    def on_interfaces_added(self, object_path, interfaces):
+    def on_interfaces_added(self, connection, sender_name, object_path, interface_name, signal_name, param):
+        path, interfaces = param.unpack()
         if 'org.freedesktop.ModemManager1.Modem' in interfaces:
-            self.on_mm_device_added(object_path, "org.freedesktop.ModemManager1")
+            self.on_mm_device_added(path, "org.freedesktop.ModemManager1")
 
     def on_timeout(self):
         self.timeout = None
-        self.err(dbus.DBusException(_("Modem Manager did not support the connection")))
+        self.err(GLib.Error(_("Modem Manager did not support the connection")))
         self.cleanup()
 
 
@@ -91,7 +91,10 @@ class NMDUNSupport(AppletPlugin):
     __priority__ = 1
 
     def on_load(self):
-        self.bus = dbus.SystemBus()
+        def on_dbus_connection_finnish(cancellable, result):
+            self._bus = Gio.bus_get_finish(result)
+
+        Gio.bus_get(Gio.BusType.SYSTEM, None, on_dbus_connection_finnish)
 
     def on_unload(self):
         pass
