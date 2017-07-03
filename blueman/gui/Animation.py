3fda0503eb9104e7fb66040ee990f4f5cb03d5e4
Sander Sweers <infirit@gmail.com>
2017-07-04 00:20:57 +0200

    Animation: Set icon names on GtkImage instead of pixbufs

--- blueman/gui/Animation.py
+++ blueman/gui/Animation.py
@@ -3,23 +3,21 @@ from gi.repository import GLib
 
 
 class Animation:
-    def __init__(self, image, images, rate=1, rev=False):
-        self.pixbuffs = []
+    def __init__(self, icon, icons, rate=1, rev=False):
+        self.icon_names = []
         self.timer = None
         self.current = 0
-        self.image = None
-
-        self.image = image
+        self.icon = icon
         self.rate = 1000 / rate
 
-        for i in range(len(images)):
-            self.pixbuffs.append(images[i])
+        for i in range(len(icons)):
+            self.icon_names.append(icons[i])
 
-        if len(self.pixbuffs) > 2 and rev:
-            ln = len(self.pixbuffs)
-            for i in range(len(self.pixbuffs)):
+        if len(self.icon_names) > 2 and rev:
+            ln = len(self.icon_names)
+            for i in range(len(self.icon_names)):
                 if i != 0 and i != ln - 1:
-                    self.pixbuffs.append(self.pixbuffs[ln - 1 - i])
+                    self.icon_names.append(self.icon_names[ln - 1 - i])
 
     def status(self):
         if self.timer:
@@ -42,15 +40,15 @@ class Animation:
     def stop(self):
         if self.timer:
             GLib.source_remove(self.timer)
-            self.image.set_from_pixbuf(self.pixbuffs[0])
+            self.icon.props.icon_name = self.icon_names[0]
             self.timer = None
 
     def _animation(self):
         self.current += 1
-        if self.current > (len(self.pixbuffs) - 1):
+        if self.current > (len(self.icon_names) - 1):
             self.current = 0
         if True:
-            self.image.set_from_pixbuf(self.pixbuffs[self.current])
+            self.icon.props.icon_name = self.icon_names[self.current]
         # print "setting " + str(self.current)
         else:
             if self.current != 0:
