dd7be66cb374a6ee0b6d7dbc20abd74031ae1908
Sander Sweers <infirit@gmail.com>
2017-07-22 13:38:59 +0200

    DBusService: Call list() on classes for QueryAvailablePlugins
    In python3 dict.keys() does not return a list and Gio is much more strict
    which caused an exception.
    
    GLib.Error: g-dbus-error-quark: GDBus.Error:org.freedesktop.DBus.Error.IOError: Method org.blueman.Applet.QueryAvailablePlugins failed with: 'dict_keys' object is not subscriptable (5)

--- blueman/plugins/applet/DBusService.py
+++ blueman/plugins/applet/DBusService.py
@@ -45,7 +45,7 @@ class DBusService(AppletPlugin):
 
     @dbus_method('org.blueman.Applet', in_signature="", out_signature="as")
     def QueryAvailablePlugins(self):
-        return self.Applet.Plugins.GetClasses().keys()
+        return list(self.Applet.Plugins.GetClasses())
 
     @dbus_method('org.blueman.Applet', in_signature="sb", out_signature="")
     def SetPluginConfig(self, plugin, value):
