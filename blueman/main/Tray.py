1af62c239b3a87db735cfa6af59abeb91795fe71
Sander Sweers <infirit@gmail.com>
2017-12-08 21:19:42 +0100

    blueman-tray: Port it to use Gio.application
    One of the key benefits is that it provides uniqueness like libunique.

--- blueman/main/Tray.py
+++ blueman/main/Tray.py
@@ -1,44 +1,61 @@
-from importlib import import_module
-
+# coding=utf-8
 import logging
-
-from blueman.Functions import check_single_instance
+from importlib import import_module
 from blueman.main.AppletService import AppletService
-
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import GLib, Gio
+from gi.repository import Gio
 
 
-class BluemanTray(object):
+class BluemanTray(Gio.Application):
     def __init__(self):
-        check_single_instance("blueman-tray")
+        super().__init__(application_id="org.blueman.Tray", flags=Gio.ApplicationFlags.FLAGS_NONE)
+        self.applet = None
+        self.indicator = None
 
-        applet = AppletService()
+    def do_startup(self):
+        Gio.Application.do_startup(self)
 
-        main_loop = GLib.MainLoop()
+        quit_action = Gio.SimpleAction.new("quit", None)
+        quit_action.connect("activate", self._on_quit)
+        self.add_action(quit_action)
+
+    def do_activate(self):
+        if self.indicator:
+            logging.info("Already an instance running")
+            return
 
         Gio.bus_watch_name(Gio.BusType.SESSION, 'org.blueman.Applet',
-                           Gio.BusNameWatcherFlags.NONE, None, lambda _connection, _name: main_loop.quit())
+                           Gio.BusNameWatcherFlags.NONE, self._on_name_appeared, self._on_name_vanished)
 
-        indicator_name = applet.GetStatusIconImplementation()
+        self.hold()
+
+    def _on_quit(self, *args):
+        logging.info("Shutting down")
+        self.release()
+
+    def _on_name_appeared(self, connection, name, owner):
+        logging.debug("%s appeared, showing indicator" % name)
+
+        self.applet = AppletService()
+        self.applet.connect('g-signal', self.on_signal)
+
+        indicator_name = self.applet.GetStatusIconImplementation()
         logging.info('Using indicator "%s"' % indicator_name)
         indicator_class = getattr(import_module('blueman.main.indicators.' + indicator_name), indicator_name)
-        self.indicator = indicator_class(applet.GetIconName(), self._activate_menu_item, self._activate_status_icon)
+        self.indicator = indicator_class(self.applet.GetIconName(), self._activate_menu_item, self._activate_status_icon)
 
-        applet.connect('g-signal', self.on_signal)
+        self.indicator.set_text(self.applet.GetText())
+        self.indicator.set_visibility(self.applet.GetVisibility())
+        self.indicator.set_menu(self.applet.GetMenu())
 
-        self.indicator.set_text(applet.GetText())
-        self.indicator.set_visibility(applet.GetVisibility())
-        self.indicator.set_menu(applet.GetMenu())
-
-        main_loop.run()
+    def _on_name_vanished(self, connection, name):
+        logging.debug("%s vanished, stopping tray" % name)
+        self._on_quit()
 
     def _activate_menu_item(self, *indexes):
-        return AppletService().ActivateMenuItem('(ai)', indexes)
+        return self.applet.ActivateMenuItem('(ai)', indexes)
 
     def _activate_status_icon(self):
-        return AppletService().Activate()
+        return self.applet.Activate()
 
     def on_signal(self, _applet, sender_name, signal_name, args):
         if signal_name == 'IconNameChanged':
