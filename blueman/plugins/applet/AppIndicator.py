57c197d731bbd346703c7b4bd445c34a09a1f45e
Sander Sweers <infirit@gmail.com>
2018-02-01 21:20:08 +0100

    AppIndicator plugin: Raise import error if typelib is not installed
    Otherwise blueman-tray will fail and not load StatusIcon instead.

--- blueman/plugins/applet/AppIndicator.py
+++ blueman/plugins/applet/AppIndicator.py
@@ -1,6 +1,13 @@
 # coding=utf-8
 from blueman.plugins.AppletPlugin import AppletPlugin
 
+# Check if Appindicator is available and raise ImportError
+from gi import require_version
+try:
+    require_version('AppIndicator3', '0.1')
+except ValueError:
+    raise ImportError("AppIndicator3 not found")
+
 
 class AppIndicator(AppletPlugin):
     __description__ = _("Uses libappindicator to show a statusicon")
