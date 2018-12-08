5cff9ccc10789ab733de2e857773dbbe306127d9
Sander Sweers <infirit@gmail.com>
2018-12-08 12:12:37 +0100

    RfKill: Use with statement

--- blueman/plugins/mechanism/RfKill.py
+++ blueman/plugins/mechanism/RfKill.py
@@ -13,6 +13,5 @@ class RfKill(MechanismPlugin):
     @dbus.service.method('org.blueman.Mechanism', in_signature="b", out_signature="", sender_keyword="caller")
     def SetRfkillState(self, state, caller):
         self.confirm_authorization(caller, "org.blueman.rfkill.setstate")
-        f = open('/dev/rfkill', 'r+b', buffering=0)
-        f.write(struct.pack("IBBBB", 0, RFKILL_TYPE_BLUETOOTH, RFKILL_OP_CHANGE_ALL, (0 if state else 1), 0))
-        f.close()
+        with open('/dev/rfkill', 'r+b', buffering=0) as f:
+            f.write(struct.pack("IBBBB", 0, RFKILL_TYPE_BLUETOOTH, RFKILL_OP_CHANGE_ALL, (0 if state else 1), 0))
