f44fd68ac8f2c236a835fde43b938ddb437bc8ab
Christopher Schramm <git@cschramm.eu>
2019-05-26 12:33:00 +0200

    Run autoconnect regularly

--- blueman/plugins/applet/AutoConnect.py
+++ blueman/plugins/applet/AutoConnect.py
@@ -1,3 +1,5 @@
+from gi.repository import GLib
+
 from blueman.Sdp import ServiceUUID
 from blueman.gui.Notification import Notification
 from blueman.plugins.AppletPlugin import AppletPlugin
@@ -14,21 +16,28 @@ class AutoConnect(AppletPlugin):
         "services": {"type": list, "default": "[]"}
     }
 
-    def on_manager_state_changed(self, state):
-        if not state:
-            return
+    def __init__(self, parent):
+        super().__init__(parent)
+        GLib.timeout_add(60000, self._run)
 
+    def on_manager_state_changed(self, state):
+        if state:
+            self._run()
+
+    def _run(self):
         for address, uuid in self.get_option('services'):
             device = self.parent.Manager.find_device(address)
-            if device is None:
-                return
+            if device is None or device.get("Connected"):
+                continue
 
-            def reply(*args):
+            def reply(*_args):
                 Notification(_("Connected"), _("Automatically connected to %(service)s on %(device)s") %
                              {"service": ServiceUUID(uuid).name, "device": device["Alias"]},
                              icon_name=device["Icon"]).show()
 
-            def err(reason):
+            def err(_reason):
                 pass
 
             self.parent.Plugins.DBusService.connect_service(device.get_object_path(), uuid, reply, err)
+
+        return True
