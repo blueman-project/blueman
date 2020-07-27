43bd9bc63e28a608a19e2e0162999391f5c46c79
Sander Sweers <infirit@gmail.com>
2020-07-27 19:10:49 +0200

    blueman-tray: Use Gio.Application

--- blueman/main/Tray.py
+++ blueman/main/Tray.py
@@ -1,25 +1,36 @@
 from importlib import import_module
 import logging
-
-from blueman.Functions import check_single_instance
+import os
+import sys
 from blueman.main.DBusProxies import AppletService
-
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import GLib, Gio
+from gi.repository import Gio
 
 
-class BluemanTray:
+class BluemanTray(Gio.Application):
     def __init__(self):
-        check_single_instance("blueman-tray")
+        super().__init__(application_id="org.blueman.Tray", flags=Gio.ApplicationFlags.FLAGS_NONE)
+        self._active = False
+
+    def do_startup(self):
+        Gio.Application.do_startup(self)
+
+        quit_action = Gio.SimpleAction.new("Quit", None)
+        quit_action.connect("activate", self.quit)
+        self.add_action(quit_action)
+
+    def do_activate(self):
+        if self._active:
+            logging.info("Already running, restarting instance")
+            os.execv(sys.argv[0], sys.argv)
+
+        Gio.bus_watch_name(Gio.BusType.SESSION, 'org.blueman.Applet', Gio.BusNameWatcherFlags.NONE,
+                           self._on_name_appeared, self._on_name_vanished)
+        self.hold()
+
+    def _on_name_appeared(self, _connection, name, _owner):
+        logging.debug("Applet started on name %s, showing indicator" % name)
 
         applet = AppletService()
-
-        main_loop = GLib.MainLoop()
-
-        Gio.bus_watch_name(Gio.BusType.SESSION, 'org.blueman.Applet',
-                           Gio.BusNameWatcherFlags.NONE, None, lambda _connection, _name: main_loop.quit())
-
         indicator_name = applet.GetStatusIconImplementation()
         logging.info(f'Using indicator "{indicator_name}"')
         indicator_class = getattr(import_module('blueman.main.indicators.' + indicator_name), indicator_name)
@@ -31,7 +42,11 @@ class BluemanTray:
         self.indicator.set_visibility(applet.GetVisibility())
         self.indicator.set_menu(applet.GetMenu())
 
-        main_loop.run()
+        self._active = True
+
+    def _on_name_vanished(self, _connection, _name):
+        logging.debug("Applet shutdown or not available at startup")
+        self.quit()
 
     def _activate_menu_item(self, *indexes):
         return AppletService().ActivateMenuItem('(ai)', indexes)
