edaf9ee59a01c84b23181886711110a6f98dd2d6
Christopher Schramm <git@cschramm.eu>
2020-08-02 15:55:25 +0200

    Add and fix type hints in RecentConns
    * Actually convert between stored and runtime items
    * Add type hints
    * Add a cast to resolve the circular type dependency issue
    * Rename some internal methods and members

--- blueman/plugins/applet/RecentConns.py
+++ blueman/plugins/applet/RecentConns.py
@@ -2,13 +2,14 @@ from gettext import gettext as _
 from operator import itemgetter
 import time
 import logging
-from typing import Dict, List, TYPE_CHECKING, Optional, Callable
+from typing import Dict, List, TYPE_CHECKING, Optional, Callable, cast
 
 from blueman.bluez.Device import Device
-from blueman.bluez.errors import DBusNoSuchAdapterError
+from blueman.bluez.errors import DBusNoSuchAdapterError, BluezDBusException
 from blueman.gui.Notification import Notification
 from blueman.Sdp import ServiceUUID
 from blueman.plugins.AppletPlugin import AppletPlugin
+from blueman.plugins.applet.PowerManager import PowerManager
 
 
 if TYPE_CHECKING:
@@ -21,33 +22,26 @@ if TYPE_CHECKING:
         sensitive: bool
         tooltip: Optional[str]
 
-    class SimpleMenuItem(MenuItemBase):
-        callback: Callable[[MenuItemBase], None]
-
-    class Item(TypedDict):
+    class _ItemBase(TypedDict):
         adapter: str
         address: str
         alias: str
         icon: str
         name: str
         uuid: str
-        time: float
-        device: str
-        mitem: Optional[SimpleMenuItem]
 
     class MenuItem(MenuItemBase):
-        callback: Callable[[Item], None]
+        callback: Callable[[_ItemBase], None]
 
+    class Item(_ItemBase):
+        time: float
+        device: Optional[str]
+        mitem: Optional[MenuItem]
+
+    class StoredIcon(_ItemBase):
+        time: str
 
 REGISTRY_VERSION = 0
-
-
-class AdapterNotFound(Exception):
-    pass
-
-
-class DeviceNotFound(Exception):
-    pass
 
 
 class RecentConns(AppletPlugin):
@@ -72,8 +66,8 @@ class RecentConns(AppletPlugin):
 
     _items = None
 
-    def on_load(self):
-        self.Adapters: Dict[str, str] = {}
+    def on_load(self) -> None:
+        self._adapters: Dict[str, str] = {}
         self.__menuitems: List["MenuItem"] = []
 
         self.item = self.parent.Plugins.Menu.add(self, 52, text=_("Recent _Connections") + "…",
@@ -82,19 +76,20 @@ class RecentConns(AppletPlugin):
 
         self.deferred = False
 
-    def store_state(self):
-        to_store = []
-        for item in self.items:
-            x = item.copy()
-            x["time"] = str(x["time"])
-            x["uuid"] = str(x["uuid"])
-            x["device"] = ''
-            x["mitem"] = ''
-            to_store.append(x)
+    def _store_state(self) -> None:
+        self.set_option("recent-connections", [
+            {
+                "adapter": item["adapter"],
+                "address": item["address"],
+                "alias": item["alias"],
+                "icon": item["icon"],
+                "name": item["name"],
+                "uuid": item["uuid"],
+                "time": str(item["time"]),
+            } for item in self.items
+        ])
 
-        self.set_option("recent-connections", to_store)
-
-    def change_sensitivity(self, sensitive):
+    def change_sensitivity(self, sensitive: bool) -> None:
         if 'PowerManager' in self.parent.Plugins.get_loaded():
             power = self.parent.Plugins.PowerManager.get_bluetooth_status()
         else:
@@ -108,16 +103,16 @@ class RecentConns(AppletPlugin):
 
         self.item.set_sensitive(sensitive)
 
-    def on_power_state_changed(self, manager, state):
+    def on_power_state_changed(self, manager: PowerManager, state: bool) -> None:
         self.change_sensitivity(state)
         if state and self.deferred:
             self.deferred = False
             self.on_manager_state_changed(state)
 
-    def on_unload(self):
+    def on_unload(self) -> None:
         self.parent.Plugins.Menu.unregister(self)
 
-    def initialize(self):
+    def initialize(self) -> None:
         logging.info("rebuilding menu")
 
         self.__menuitems = []
@@ -139,7 +134,7 @@ class RecentConns(AppletPlugin):
                 self.add_item(item)
                 count += 1
 
-    def on_manager_state_changed(self, state):
+    def on_manager_state_changed(self, state: bool) -> None:
         if state:
             if 'PowerManager' in self.parent.Plugins.get_loaded():
                 if not self.parent.Plugins.PowerManager.get_bluetooth_status():
@@ -150,15 +145,13 @@ class RecentConns(AppletPlugin):
             self.item.set_sensitive(True)
             adapters = self.parent.Manager.get_adapters()
 
-            self.Adapters = {}
-            for adapter in adapters:
-                self.Adapters[str(adapter.get_object_path())] = str(adapter["Address"])
+            self._adapters = {adapter.get_object_path(): adapter["Address"]
+                              for adapter in adapters}
 
-            for i in reversed(self.items):
-                try:
-                    i["device"] = self.get_device_path(i)
-                except (AdapterNotFound, DeviceNotFound):
-                    pass
+            for i in self.items:
+                device_path = self._get_device_path(i["adapter"], i["address"])
+                if device_path:
+                    i["device"] = device_path
 
             self.initialize()
         else:
@@ -167,44 +160,45 @@ class RecentConns(AppletPlugin):
 
         self.change_sensitivity(state)
 
-    def on_device_removed(self, path):
+    def on_device_removed(self, path: str) -> None:
         for item in reversed(self.items):
             if item['device'] == path:
                 self.items.remove(item)
                 self.initialize()
 
-    def on_adapter_added(self, path):
+    def on_adapter_added(self, path: str) -> None:
         a = self.parent.Manager.get_adapter(path)
-        self.Adapters[path] = a["Address"]
+        self._adapters[path] = a["Address"]
         self.initialize()
 
-    def on_adapter_removed(self, path):
-        if str(path) in self.Adapters:
-            del self.Adapters[str(path)]
+    def on_adapter_removed(self, path: str) -> None:
+        if path in self._adapters:
+            del self._adapters[path]
         else:
             logging.warning("Adapter not found in list")
 
         self.initialize()
 
-    def notify(self, object_path, uuid):
+    def notify(self, object_path: str, uuid: str) -> None:
         device = Device(obj_path=object_path)
         logging.info(f"{device} {uuid}")
-        item = {}
         try:
             adapter = self.parent.Manager.get_adapter(device['Adapter'])
         except DBusNoSuchAdapterError:
             logging.warning("adapter not found")
             return
 
-        item["adapter"] = adapter["Address"]
-        item["address"] = device['Address']
-        item["alias"] = device['Alias']
-        item["icon"] = device['Icon']
-        item["name"] = ServiceUUID(uuid).name
-        item["uuid"] = uuid
-        item["time"] = time.time()
-        item["device"] = object_path
-        item["mitem"] = None  # menu item object
+        item: "Item" = {
+            "adapter": adapter["Address"],
+            "address": device['Address'],
+            "alias": device['Alias'],
+            "icon": device['Icon'],
+            "name": ServiceUUID(uuid).name,
+            "uuid": uuid,
+            "time": time.time(),
+            "device": object_path,
+            "mitem": None
+        }
 
         for i in self.items:
             if i["adapter"] == item["adapter"] and \
@@ -219,46 +213,51 @@ class RecentConns(AppletPlugin):
         self.items.append(item)
         self.initialize()
 
-        self.store_state()
+        self._store_state()
 
-    def on_item_activated(self, item):
+    def on_item_activated(self, item: "Item") -> None:
         logging.info(f"Connect {item['address']} {item['uuid']}")
+
+        assert item["mitem"] is not None
 
         item["mitem"]["sensitive"] = False
         self.parent.Plugins.Menu.on_menu_changed()
 
-        def reply(*args):
+        def reply() -> None:
+            assert item["mitem"] is not None
             Notification(_("Connected"), _("Connected to %s") % item["mitem"]["text"],
                          icon_name=item["icon"]).show()
             item["mitem"]["sensitive"] = True
             self.parent.Plugins.Menu.on_menu_changed()
 
-        def err(reason):
+        def err(reason: BluezDBusException) -> None:
             Notification(_("Failed to connect"), str(reason).split(": ")[-1],
                          icon_name="dialog-error").show()
+            assert item["mitem"] is not None
             item["mitem"]["sensitive"] = True
             self.parent.Plugins.Menu.on_menu_changed()
 
         self.parent.Plugins.DBusService.connect_service(item["device"], item["uuid"], reply, err)
 
-    def add_item(self, item):
-        if item["adapter"] not in self.Adapters.values():
+    def add_item(self, item: "Item") -> None:
+        if item["adapter"] not in self._adapters.values():
             item["device"] = None
-        elif not item["device"] and item["adapter"] in self.Adapters.values():
-            try:
-                item["device"] = self.get_device_path(item)
-
-            except (AdapterNotFound, DeviceNotFound):
+        elif not item["device"] and item["adapter"] in self._adapters.values():
+            device = self._get_device_path(item["adapter"], (item["address"]))
+            if device is not None:
+                item["device"] = device
+            else:
                 self.items.remove(item)
                 self.initialize()
 
         mitem: "MenuItem" = {
             "text": _("%(service)s on %(device)s") % {"service": item["name"], "device": item["alias"]},
             "markup": True,
-            "icon_name": item["mitem"]["icon_name"] if item["mitem"] else item["icon"],
+            "icon_name": item["mitem"]["icon_name"] if item["mitem"] is not None else item["icon"],
             "sensitive": item["device"] is not None,
             "tooltip": None if item["device"] is None else _("Adapter for this connection is not available"),
-            "callback": item["mitem"]["callback"] if item["mitem"] else lambda itm=item: self.on_item_activated(itm)
+            "callback": (item["mitem"]["callback"] if item["mitem"] is not None
+                         else cast(Callable[["_ItemBase"], None], lambda itm=item: self.on_item_activated(itm)))
         }
 
         item["mitem"] = mitem
@@ -266,23 +265,20 @@ class RecentConns(AppletPlugin):
         self.__menuitems.append(mitem)
         self.parent.Plugins.Menu.on_menu_changed()
 
-    def get_menu(self):
+    def get_menu(self) -> List["MenuItem"]:
         return self.__menuitems
 
-    def get_device_path(self, item):
+    def _get_device_path(self, adapter_path: str, address: str) -> Optional[str]:
         try:
-            adapter = self.parent.Manager.get_adapter(item["adapter"])
+            adapter = self.parent.Manager.get_adapter(adapter_path)
         except DBusNoSuchAdapterError:
-            raise AdapterNotFound
+            return None
 
-        device = self.parent.Manager.find_device(item["address"], adapter.get_object_path())
-        if device is None:
-            raise DeviceNotFound
-        else:
-            return device.get_object_path()
+        device = self.parent.Manager.find_device(address, adapter.get_object_path())
+        return device.get_object_path() if device is not None else None
 
     @property
-    def items(self):
+    def items(self) -> List["Item"]:
         if self._items is not None:
             return self._items
 
@@ -292,18 +288,21 @@ class RecentConns(AppletPlugin):
             self._items = []
             return self._items
 
-        for i in reversed(items):
-            if "name" not in i or "uuid" not in i:
-                items.remove(i)
-            try:
-                i["device"] = self.get_device_path(i)
-            except AdapterNotFound:
-                i["device"] = None
-            except DeviceNotFound:
-                items.remove(i)
-
-            i["time"] = float(i["time"])
-
-        self._items = items
+        self._items = [
+            {
+                "adapter": i["adapter"],
+                "address": i["address"],
+                "alias": i["alias"],
+                "icon": i["icon"],
+                "name": i["name"],
+                "uuid": i["uuid"],
+                "time": float(i["time"]),
+                "device": (self._get_device_path(i["adapter"], i["address"])
+                           if i["adapter"] in self._adapters.values() else None),
+                "mitem": None
+            }
+            for i in items
+            if i["adapter"] not in self._adapters.values() or self._get_device_path(i["adapter"], i["address"])
+        ]
 
         return self._items
