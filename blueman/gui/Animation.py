c7d62881f5d32abe2b125cfc55d83e1a92a78ac3
Christopher Schramm <git@cschramm.eu>
2020-08-16 00:57:04 +0200

    Clean up Animation
    * Drop unused rev argument
    * Simplify constructor
    * Drop unused and unnecessary get_rate
    * Remove dead code

--- blueman/gui/Animation.py
+++ blueman/gui/Animation.py
@@ -1,33 +1,19 @@
-from typing import List
-
 from gi.repository import GLib
 
 
 class Animation:
-    def __init__(self, icon, icons, rate=1, rev=False):
-        self.icon_names: List[str] = []
+    def __init__(self, icon, icons, rate=1):
+        self.icon_names = list(icons)
         self.timer = None
         self.current = 0
         self.icon = icon
         self.rate = 1000 / rate
-
-        for i in range(len(icons)):
-            self.icon_names.append(icons[i])
-
-        if len(self.icon_names) > 2 and rev:
-            ln = len(self.icon_names)
-            for i in range(len(self.icon_names)):
-                if i != 0 and i != ln - 1:
-                    self.icon_names.append(self.icon_names[ln - 1 - i])
 
     def status(self):
         if self.timer:
             return True
         else:
             return False
-
-    def get_rate(self):
-        return self.rate * 1000
 
     def set_rate(self, rate):
         if not self.rate == (1000 / rate):
@@ -48,12 +34,6 @@ class Animation:
         self.current += 1
         if self.current > (len(self.icon_names) - 1):
             self.current = 0
-        if True:
-            self.icon.props.icon_name = self.icon_names[self.current]
-        # print "setting " + str(self.current)
-        else:
-            if self.current != 0:
-                # self.image.set_from_pixbuf(self.pixbuffs[0])
-                self.current = 0
+        self.icon.props.icon_name = self.icon_names[self.current]
 
-        return 1
+        return True
