blueman-project/blueman

commit cd9eb76127f9f531917db5db1b35b457021e0609

Do not use disconnect_by_func
It only disconnects the first it finds. We can however can call
it more than once to disconnecy signal handlers but then we may
as well track signal handler ids. See pygobject bug 106
https://gitlab.gnome.org/GNOME/pygobject/issues/106

blueman/main/applet/BluezAgent.py
@@ -6,6 +6,7 @@ from gettext import gettext as _
 from html import escape
 import random
 from xml.etree import ElementTree
+from typing import Dict
 
 from blueman.bluez.Device import Device
 from blueman.bluez.AgentManager import AgentManager
@@ -92,6 +93,7 @@ class BluezAgent(DbusService):
 
         self.dialog = None
         self._db = None
+        self._devhandlerids: Dict[str, int] = {}
 
     def register_agent(self):
         logging.info("Register Agent")
@@ -197,7 +199,8 @@ class BluezAgent(DbusService):
     # Workaround BlueZ not calling the Cancel method, see #164
     def _on_device_property_changed(self, device, key, value, path):
         if (key == "Paired" and value) or (key == "Connected" and not value):
-            device.disconnect_by_func(self._on_device_property_changed)
+            handlerid = self._devhandlerids.pop(path)
+            device.disconnect(handlerid)
             self._on_cancel()
 
     def _on_release(self):
@@ -239,7 +242,7 @@ class BluezAgent(DbusService):
     def _on_display_passkey(self, device, passkey, _entered):
         logging.info('DisplayPasskey (%s, %d)' % (device, passkey))
         dev = Device(device)
-        dev.connect_signal("property-changed", self._on_device_property_changed)
+        self._devhandlerids[device] = dev.connect_signal("property-changed", self._on_device_property_changed)
 
         notify_message = _("Pairing passkey for") + " %s: %s" % (self.get_device_string(device), passkey)
         self.n = Notification("Bluetooth", notify_message, 0, icon_name="blueman")
@@ -248,7 +251,7 @@ class BluezAgent(DbusService):
     def _on_display_pin_code(self, device, pin_code):
         logging.info('DisplayPinCode (%s, %s)' % (device, pin_code))
         dev = Device(device)
-        dev.connect_signal("property-changed", self._on_device_property_changed)
+        self._devhandlerids[device] = dev.connect_signal("property-changed", self._on_device_property_changed)
 
         notify_message = _("Pairing PIN code for") + " %s: %s" % (self.get_device_string(device), pin_code)
         self.n = Notification("Bluetooth", notify_message, 0, icon_name="blueman")

blueman/bluez/obex/Manager.py
@@ -31,14 +31,16 @@ class Manager(GObject.GObject, metaclass=SingletonGObjectMeta):
             Gio.BusType.SESSION, Gio.DBusObjectManagerClientFlags.NONE,
             self.__bus_name, '/', None, None, None)
 
-        self._object_manager.connect('object-added', self._on_object_added)
-        self._object_manager.connect('object-removed', self._on_object_removed)
+        self._manager_handlerids: List[int] = []
+        self._manager_handlerids.append(self._object_manager.connect('object-added', self._on_object_added))
+        self._manager_handlerids.append(self._object_manager.connect('object-removed', self._on_object_removed))
 
         weakref.finalize(self, self._on_delete)
 
     def _on_delete(self) -> None:
-        self._object_manager.disconnect_by_func(self._on_object_added)
-        self._object_manager.disconnect_by_func(self._on_object_removed)
+        for handlerid in self._manager_handlerids:
+            self._object_manager.disconnect(handlerid)
+        self._manager_handlerids = []
 
     def _on_object_added(self, _object_manager: Gio.DBusObjectManager, dbus_object: Gio.DBusObject) -> None:
         session_proxy = dbus_object.get_interface('org.bluez.obex.Session1')
@@ -66,8 +68,9 @@ class Manager(GObject.GObject, metaclass=SingletonGObjectMeta):
             logging.info(object_path)
             transfer = self.__transfers.pop(object_path)
 
-            transfer.disconnect_by_func(self._on_transfer_completed, True)
-            transfer.disconnect_by_func(self._on_transfer_completed, False)
+            # Disconnect as many times as we connect (pygobject bug #106)
+            transfer.disconnect_by_func(self._on_transfer_completed)
+            transfer.disconnect_by_func(self._on_transfer_completed)
 
         if session_proxy:
             logging.info(object_path)

blueman/gui/DeviceList.py
@@ -58,13 +58,18 @@ class DeviceList(GenericList):
         self.monitored_devices: List[str] = []
 
         self.manager = Manager()
-        self.manager.connect_signal('adapter-removed', self.__on_manager_signal, 'adapter-removed')
-        self.manager.connect_signal('adapter-added', self.__on_manager_signal, 'adapter-added')
-        self.manager.connect_signal('device-created', self.__on_manager_signal, 'device-created')
-        self.manager.connect_signal('device-removed', self.__on_manager_signal, 'device-removed')
+        self._managerhandlers: List[int] = []
+        self._managerhandlers.append(self.manager.connect_signal('adapter-removed', self.__on_manager_signal,
+                                                                 'adapter-removed'))
+        self._managerhandlers.append(self.manager.connect_signal('adapter-added', self.__on_manager_signal,
+                                                                 'adapter-added'))
+        self._managerhandlers.append(self.manager.connect_signal('device-created', self.__on_manager_signal,
+                                                                 'device-created'))
+        self._managerhandlers.append(self.manager.connect_signal('device-removed', self.__on_manager_signal,
+                                                                 'device-removed'))
 
         self.any_device = AnyDevice()
-        self.any_device.connect_signal("property-changed", self._on_device_property_changed)
+        self._anydevhandler = self.any_device.connect_signal("property-changed", self._on_device_property_changed)
 
         self.__discovery_time = 0
         self.__adapter_path = None
@@ -82,9 +87,9 @@ class DeviceList(GenericList):
 
         self.set_adapter(adapter_name)
         self._any_adapter = AnyAdapter()
-        self._any_adapter.connect_signal("property-changed", self._on_property_changed)
+        self._anyadapterhandler = self._any_adapter.connect_signal("property-changed", self._on_property_changed)
 
-        self.selection.connect('changed', self.on_selection_changed)
+        self._selectionhandler = self.selection.connect('changed', self.on_selection_changed)
 
         self.icon_theme = Gtk.IconTheme.get_default()
         self.icon_theme.prepend_search_path(ICON_PATH)
@@ -92,10 +97,11 @@ class DeviceList(GenericList):
         self.icon_theme.connect("changed", self.on_icon_theme_changed)
 
     def destroy(self):
-        self.any_device.disconnect_by_func(self._on_device_property_changed)
-        self._any_adapter.disconnect_by_func(self._on_property_changed)
-        self.selection.disconnect_by_func(self.on_selection_changed)
-        self.manager.disconnect_by_func(self.__on_manager_signal)
+        self.any_device.disconnect(self._anydevhandler)
+        self._any_adapter.disconnect(self._anyadapterhandler)
+        self.selection.disconnect(self._selectionhandler)
+        for handler in self._managerhandlers:
+            self.manager.disconnect(handler)
         super().destroy()
 
     def __on_manager_signal(self, manager, path, signal_name):

blueman/gui/GtkAnimation.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from typing import List
+from typing import List, Optional
 
 import cairo
 import gi
@@ -126,14 +126,16 @@ class TreeRowFade(AnimBase):
         super().__init__(1.0)
         self.tw = tw
 
-        self.tw.connect_after("draw", self.on_draw)
+        self.sig: Optional[int] = self.tw.connect_after("draw", self.on_draw)
 
         self.row = Gtk.TreeRowReference.new(tw.props.model, path)
         self.stylecontext = tw.get_style_context()
         self.columns = columns
 
     def unref(self):
-        self.tw.disconnect_by_func(self.on_draw)
+        if self.sig is not None:
+            self.tw.disconnect(self.sig)
+            self.sig = None
 
     def get_iter(self):
         return self.tw.props.model.get_iter(self.row.get_path())
@@ -143,7 +145,8 @@ class TreeRowFade(AnimBase):
             return
 
         if not self.row.valid():
-            self.tw.disconnect_by_func(self.on_draw)
+            self.tw.disconnect(self.sig)
+            self.sig = None
             return
 
         path = self.row.get_path()
@@ -183,7 +186,8 @@ class TreeRowColorFade(TreeRowFade):
             return
 
         if not self.row.valid():
-            self.tw.disconnect_by_func(self.on_draw)
+            self.tw.disconnect(self.sig)
+            self.sig = None
             return
 
         path = self.row.get_path()
@@ -208,7 +212,7 @@ class CellFade(AnimBase):
         self.tw = tw
 
         self.frozen = False
-        tw.connect_after("draw", self.on_draw)
+        self.sig = tw.connect_after("draw", self.on_draw)
         self.row = Gtk.TreeRowReference.new(tw.props.model, path)
         self.selection = tw.get_selection()
         self.columns: List[Gtk.TreeViewColumn] = []
@@ -216,7 +220,9 @@ class CellFade(AnimBase):
             self.columns.append(self.tw.get_column(i))
 
     def unref(self):
-        self.tw.disconnect_by_func(self.on_draw)
+        if self.sig is not None:
+            self.tw.disconnect(self.sig)
+            self.sig = None
 
     def get_iter(self):
         return self.tw.props.model.get_iter(self.row.get_path())
@@ -226,8 +232,8 @@ class CellFade(AnimBase):
             return
 
         if not self.row.valid():
-            self.tw.disconnect_by_func(self.on_draw)
-            return
+            self.tw.disconnect(self.sig)
+            self.sig = None
 
         path = self.row.get_path()
 
@@ -269,7 +275,7 @@ class WidgetFade(AnimBase):
         self.widget = widget
         self.color = color
 
-        widget.connect_after("draw", self.on_draw)
+        self.sig = widget.connect_after("draw", self.on_draw)
 
     def on_draw(self, widget, cr):
         if not self.frozen:

blueman/gui/MessageArea.py
@@ -64,7 +64,7 @@ class MessageArea(Gtk.InfoBar):
 
     def _show_message(self, text, bt=None, icon="dialog-warning"):
         def on_finished(anim):
-            anim.disconnect_by_func(on_finished)
+            anim.disconnect(sig)
             anim.freeze()
 
         self.text = text
@@ -81,12 +81,12 @@ class MessageArea(Gtk.InfoBar):
             self.hl_anim.color = Gdk.RGBA(0, 0, 1, 1)
 
         if not self.props.visible:
-            self.anim.connect("animation-finished", on_finished)
+            sig = self.anim.connect("animation-finished", on_finished)
             self.anim.thaw()
             self.show()
             self.anim.animate(start=0.0, end=1.0, duration=500)
         else:
-            self.hl_anim.connect("animation-finished", on_finished)
+            sig = self.hl_anim.connect("animation-finished", on_finished)
             self.hl_anim.thaw()
             self.hl_anim.animate(start=0.7, end=1.0, duration=1000)
 

blueman/gui/applet/PluginDialog.py
@@ -154,8 +154,8 @@ class PluginDialog(Gtk.Window):
 
         self.populate()
 
-        self.applet.Plugins.connect("plugin-loaded", self.plugin_state_changed, True)
-        self.applet.Plugins.connect("plugin-unloaded", self.plugin_state_changed, False)
+        self.sig_a: int = self.applet.Plugins.connect("plugin-loaded", self.plugin_state_changed, True)
+        self.sig_b: int = self.applet.Plugins.connect("plugin-unloaded", self.plugin_state_changed, False)
         self.connect("delete-event", self._on_close)
 
         self.list.set_cursor(0)
@@ -179,8 +179,8 @@ class PluginDialog(Gtk.Window):
                 return 1
 
     def _on_close(self, *args, **kwargs):
-        self.applet.Plugins.disconnect_by_func(self.plugin_state_changed, True)
-        self.applet.Plugins.disconnect_by_func(self.plugin_state_changed, False)
+        self.applet.Plugins.disconnect(self.sig_a)
+        self.applet.Plugins.disconnect(self.sig_b)
 
     def on_selection_changed(self, selection):
         model, tree_iter = selection.get_selected()

blueman/gui/manager/ManagerDeviceList.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from gettext import gettext as _
+from typing import Optional
 import html
 import logging
 import cairo
@@ -83,6 +84,8 @@ class ManagerDeviceList(DeviceList):
         Gtk.Widget.drag_dest_add_uri_targets(self)
 
         self.set_search_equal_func(self.search_func, None)
+
+        self._faderhandler: Optional[int] = None
 
     def _on_settings_changed(self, settings, key):
         if key in ('sort-by', 'sort-order'):
@@ -218,13 +221,14 @@ class ManagerDeviceList(DeviceList):
         tree_iter = self.find_device(device)
 
         row_fader = self.get(tree_iter, "row_fader")["row_fader"]
-        row_fader.connect("animation-finished", self.__on_fader_finished, device, tree_iter)
+        self._faderhandler = row_fader.connect("animation-finished", self.__on_fader_finished, device, tree_iter)
         row_fader.thaw()
         self.emit("device-selected", None, None)
         row_fader.animate(start=row_fader.get_state(), end=0.0, duration=400)
 
     def __on_fader_finished(self, fader, device, tree_iter):
-        fader.disconnect_by_func(self.__on_fader_finished)
+        fader.disconnect(self._faderhandler)
+        self._faderhandler = None
         fader.freeze()
         super().device_remove_event(device)
 
@@ -254,10 +258,10 @@ class ManagerDeviceList(DeviceList):
             cell_fader.freeze()
 
             def on_finished(fader):
-                fader.disconnect_by_func(on_finished)
+                fader.disconnect(faderhandler)
                 fader.freeze()
 
-            row_fader.connect("animation-finished", on_finished)
+            faderhandler = row_fader.connect("animation-finished", on_finished)
             row_fader.set_state(0.0)
             row_fader.animate(start=0.0, end=1.0, duration=500)
 
@@ -367,9 +371,9 @@ class ManagerDeviceList(DeviceList):
 
                 def on_finished(fader):
                     fader.freeze()
-                    fader.disconnect_by_func(on_finished)
+                    fader.disconnect(faderhandler)
 
-                fader.connect("animation-finished", on_finished)
+                faderhandler = fader.connect("animation-finished", on_finished)
 
             to_store = {}
             if round(row["rssi"], -1) != round(rssi_perc, -1):
@@ -404,12 +408,12 @@ class ManagerDeviceList(DeviceList):
                 fader.animate(start=fader.get_state(), end=0.0, duration=400)
 
                 def on_finished(fader):
-                    fader.disconnect_by_func(on_finished)
+                    fader.disconnect(faderhandler)
                     fader.freeze()
                     if row_ref.valid():
                         self.set(tree_iter, rssi_pb=None, lq_pb=None, tpl_pb=None)
 
-                fader.connect("animation-finished", on_finished)
+                faderhandler = fader.connect("animation-finished", on_finished)
 
     def tooltip_query(self, tw, x, y, kb, tooltip):
         path = self.get_path_at_pos(x, y)

blueman/gui/manager/ManagerMenu.py
@@ -245,8 +245,8 @@ class ManagerMenu:
         item.show()
         self._adapters_group = item.get_group()
 
-        item.connect("activate", self.on_adapter_selected, object_path)
-        adapter.connect_signal("property-changed", self.on_adapter_property_changed)
+        self._itemhandler = item.connect("activate", self.on_adapter_selected, object_path)
+        self._adapterhandler = adapter.connect_signal("property-changed", self.on_adapter_property_changed)
 
         menu.insert(item, self._insert_adapter_item_pos)
         self._insert_adapter_item_pos += 1
@@ -263,8 +263,8 @@ class ManagerMenu:
         item, adapter = self.adapter_items.pop(adapter_path)
         menu = self.item_adapter.get_submenu()
 
-        item.disconnect_by_func(self.on_adapter_selected, adapter.get_object_path())
-        adapter.disconnect_by_func(self.on_adapter_property_changed)
+        item.disconnect(self._itemhandler)
+        adapter.disconnect(self._adapterhandler)
 
         menu.remove(item)
         self._insert_adapter_item_pos -= 1

commit 0e2f683ad4cdaa423e98da2ceceb7a4db94e3b11

Fix issues in BluezAgent
* Call correct method to disconnect signal (broken since cd9eb76127f9f531917db5db1b35b457021e0609).
* Return default PIN correctly (broken since a1c81b6c4ca56f2495e074dffd2519c667db6c62).

blueman/main/applet/BluezAgent.py
@@ -195,7 +195,7 @@ class BluezAgent(DbusService):
     def _on_device_property_changed(self, device, key, value, path):
         if (key == "Paired" and value) or (key == "Connected" and not value):
             handlerid = self._devhandlerids.pop(path)
-            device.disconnect(handlerid)
+            device.disconnect_signal(handlerid)
             self._on_cancel()
 
     def _on_release(self):
@@ -220,7 +220,8 @@ class BluezAgent(DbusService):
         default_pin = self._lookup_default_pin(device_path)
         if default_pin is not None:
             logging.info(f"Sending default pin: {default_pin}")
-            return default_pin
+            ok(default_pin)
+            return
 
         self.ask_passkey(dialog_msg, notify_msg, False, True, device_path, ok, err)
         if self.dialog: