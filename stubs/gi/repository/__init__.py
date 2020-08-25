d60cfb2085ef2a059eb33e1fa27dfd57fc552b77
Christopher Schramm <git@cschramm.eu>
2020-08-26 01:46:02 +0200

    Add pgi-docgen auto-generated GdkX11 stubs

--- stubs/gi/repository/__init__.py
+++ stubs/gi/repository/__init__.py
@@ -1,6 +1,5 @@
 from typing import Any
 
-GdkX11: Any
 Gtk: Any
 AppIndicator3: Any
 NM: Any
---
+++ stubs/gi/repository/xlib.pyi
@@ -0,0 +1,49 @@
+import builtins
+import typing
+
+from gi.repository import GObject
+
+
+class Display():
+    ...
+
+
+class Screen():
+    ...
+
+
+class Visual():
+    ...
+
+
+class XConfigureEvent():
+    ...
+
+
+class XFontStruct():
+    ...
+
+
+class XImage():
+    ...
+
+
+class XTrapezoid():
+    ...
+
+
+class XVisualInfo():
+    ...
+
+
+class XWindowAttributes():
+    ...
+
+
+class XEvent():
+    ...
+
+
+def open_display() -> None: ...
+
+
