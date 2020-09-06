b4f0738d5a9a360e6f8fef0e2a0bc9d4f9b38383
Christopher Schramm <git@cschramm.eu>
2020-09-06 09:52:45 +0200

    Actually load all packages in tests

--- test/test_imports.py
+++ test/test_imports.py
@@ -2,6 +2,12 @@ import os.path
 import pkgutil
 from unittest import TestCase, TestSuite
 
+from gi.repository import Gdk, GdkX11
+
+
+fake_x11_screen = GdkX11.X11Screen()
+Gdk.Screen.get_default = lambda: fake_x11_screen
+
 
 class TestImports(TestCase):
     def __init__(self, mod_name):
@@ -13,7 +19,7 @@ class TestImports(TestCase):
 def load_tests(*_args):
     test_cases = TestSuite()
     home = os.path.dirname(os.path.dirname(__file__))
-    for package in pkgutil.iter_modules([f"{home}/blueman"], "blueman."):
+    for package in pkgutil.walk_packages([f"{home}/blueman"], "blueman."):
         test_cases.addTest(TestImports(package.name))
 
     assert test_cases.countTestCases() > 0
