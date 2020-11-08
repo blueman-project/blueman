499f4527c267fc4f6f0eb6dd4fd336d3d0af53bd
Christopher Schramm <git@cschramm.eu>
2020-11-08 23:46:34 +0100

    Avoid generic connection notification if battery notification is shown
    This assumes that ServicesResolved and Connected are more or less closely coupled which I am not sure if it is true for all kinds on devices but hope it holds.

--- blueman/plugins/applet/ConnectionNotifier.py
+++ blueman/plugins/applet/ConnectionNotifier.py
@@ -4,6 +4,7 @@ from typing import Any
 from blueman.bluez.Device import Device
 from blueman.gui.Notification import Notification
 from blueman.plugins.AppletPlugin import AppletPlugin
+from blueman.plugins.applet.Battery import Battery
 
 
 class ConnectionNotifier(AppletPlugin):
@@ -14,5 +15,8 @@ class ConnectionNotifier(AppletPlugin):
     def on_device_property_changed(self, path: str, key: str, value: Any) -> None:
         if key == "Connected":
             device = Device(obj_path=path)
+            if value and "Battery" in self.parent.Plugins.get_loaded() and Battery.applicable(device):
+                return
+
             Notification(device["Alias"], _("Connected") if value else _("Disconnected"),
                          icon_name=device["Icon"]).show()
