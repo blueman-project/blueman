blueman-project/blueman

commit 8c8346c8ddce315f3357ecd31469ac96646a1404

Fix device removal handling
This fixes multiple issues:
- if two devices are removed within the time the fader animation takes,
  _self._faderhandler is overwritten with the faderhandler id of the latter
  fader, which leads to non-removal of the first fader handler
- if two devices are removed within the time the fader animation takes,
  _self._faderhandler is set to None by the first __on_fader_finished
  callback, leading to a crash in the second callback because
  disconnect is called as `fader.disconnect(None)`
- If /org/bluez/hci0/dev_XX is removed, and then /org/bluez/hci0 is
  removed within the time the fader animation takes, the removal of
  /org/bluez/hci0 will lead to a second duplicate device_remove_event for
  /org/bluez/hci0/dev_XX, as it's not been removed from the liststore

blueman/gui/manager/ManagerDeviceList.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from gettext import gettext as _
-from typing import Optional
+from typing import Dict
 import html
 import logging
 import cairo
@@ -85,7 +85,7 @@ class ManagerDeviceList(DeviceList):
 
         self.set_search_equal_func(self.search_func, None)
 
-        self._faderhandler: Optional[int] = None
+        self._faderhandlers: Dict[str, int] = {}
 
     def _on_settings_changed(self, settings, key):
         if key in ('sort-by', 'sort-order'):
@@ -221,16 +221,18 @@ class ManagerDeviceList(DeviceList):
         tree_iter = self.find_device(device)
 
         row_fader = self.get(tree_iter, "row_fader")["row_fader"]
-        self._faderhandler = row_fader.connect("animation-finished", self.__on_fader_finished, device, tree_iter)
+        super().device_remove_event(device)
+        self._faderhandlers.update({
+            device.get_object_path(): row_fader.connect("animation-finished", self.__on_fader_finished, device)
+        })
+
         row_fader.thaw()
         self.emit("device-selected", None, None)
         row_fader.animate(start=row_fader.get_state(), end=0.0, duration=400)
 
-    def __on_fader_finished(self, fader, device, tree_iter):
-        fader.disconnect(self._faderhandler)
-        self._faderhandler = None
+    def __on_fader_finished(self, fader, device):
+        fader.disconnect(self._faderhandlers.pop(device.get_object_path()))
         fader.freeze()
-        super().device_remove_event(device)
 
     def device_add_event(self, device):
         self.add_device(device)

commit 2d2c8b88be2836bfcc9f61347141b78f914ce20c

Fix fader for device removal.
Broken in 8c8346c8ddce315f3357ecd31469ac96646a1404

blueman/gui/manager/ManagerDeviceList.py
@@ -258,9 +258,12 @@ class ManagerDeviceList(DeviceList):
         assert tree_iter is not None
 
         row_fader = self.get(tree_iter, "row_fader")["row_fader"]
+        self._prepare_fader(row_fader, lambda: self.__fader_finished(device))
+        row_fader.animate(start=row_fader.get_state(), end=0.0, duration=400)
+
+    def __fader_finished(self, device: Device) -> None:
         super().device_remove_event(device)
         self.emit("device-selected", None, None)
-        self._prepare_fader(row_fader).animate(start=row_fader.get_state(), end=0.0, duration=400)
 
     def device_add_event(self, device: Device) -> None:
         self.add_device(device)