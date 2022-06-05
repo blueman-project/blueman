458c263553f9940dac5ad57ddcfc55cd57f7c096
Sander Sweers <infirit@gmail.com>
2022-06-05 21:28:35 +0200

    ConnectionNotifier: Rework the battery handling
    * Add a delay as the Battery1 interface may not be there yet (also #1696)
    * Move Battery plugin into ConnectionNotifier

--- blueman/plugins/applet/ConnectionNotifier.py
+++ blueman/plugins/applet/ConnectionNotifier.py
@@ -1,10 +1,13 @@
+import logging
 from gettext import gettext as _
 from typing import Any
 
 from blueman.bluez.Device import Device
+from blueman.bluez.Battery import Battery
+from blueman.bluez.errors import BluezDBusException
 from blueman.gui.Notification import Notification
 from blueman.plugins.AppletPlugin import AppletPlugin
-from blueman.plugins.applet.Battery import Battery
+from gi.repository import GLib
 
 
 class ConnectionNotifier(AppletPlugin):
@@ -13,10 +16,25 @@ class ConnectionNotifier(AppletPlugin):
     __description__ = _("Shows desktop notifications when devices get connected or disconnected.")
 
     def on_device_property_changed(self, path: str, key: str, value: Any) -> None:
-        if key == "Connected":
-            device = Device(obj_path=path)
-            if value and "Battery" in self.parent.Plugins.get_loaded() and Battery.applicable(device):
-                return
+        def show_notification() -> bool:
+            try:
+                perc = f"{battery['Percentage']}%"
+                icon_name = "battery"
+            except BluezDBusException:
+                perc = None
+                icon_name = device["Icon"]
+                logging.debug("Failed to get battery level")
 
-            Notification(device["Alias"], _("Connected") if value else _("Disconnected"),
-                         icon_name=device["Icon"]).show()
+            txt = f"{_('Connected')} {perc if perc is not None else ''}"
+            Notification(device["Alias"], txt, icon_name=icon_name).show()
+            return False
+
+        device = Device(obj_path=path)
+        battery = Battery(obj_path=path)
+
+        if key == "Connected":
+            if value:
+                # FIXME delay is needed for battery info. Most notably xbox one pads #1696
+                GLib.timeout_add_seconds(5, show_notification)
+            else:
+                Notification(device["Alias"], _('Disconnected'), icon_name=device["Icon"]).show()
