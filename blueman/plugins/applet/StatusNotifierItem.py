c0d50876d9d1989cefdf407bd4780e811dcbf190
Christopher Schramm <git@cschramm.eu>
2022-10-12 12:33:52 +0200

    Fix dependencies on StatusIcon plugin
    AudioProfiles and StatusNotifierItem do not actually depend on it. They are rather pointless without a status icon but they do work. Also there is no point on not making AudioProfiles unloadable (the default). This makes #1496 actually work.

--- blueman/plugins/applet/StatusNotifierItem.py
+++ blueman/plugins/applet/StatusNotifierItem.py
@@ -9,7 +9,6 @@ from blueman.plugins.applet.StatusIcon import StatusIconImplementationProvider
 class StatusNotifierItem(AppletPlugin, StatusIconImplementationProvider):
     __description__ = _("Provides a StatusNotifierItem to show a statusicon")
     __icon__ = "bluetooth-symbolic"
-    __depends__ = ['StatusIcon']
 
     def on_query_status_icon_implementation(self) -> Tuple[str, int]:
         return "StatusNotifierItem", 20
