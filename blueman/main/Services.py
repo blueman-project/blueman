e64d7e3ccd39ea6f0a27b091f6dc9113d867b91a
Sander Sweers <infirit@gmail.com>
2022-12-10 19:18:34 +0100

    Services: Add accels to close

--- blueman/main/Services.py
+++ blueman/main/Services.py
@@ -29,6 +29,10 @@ class BluemanServices(Gtk.Application):
         s.set_callback(do_quit)
         s.attach()
 
+        close_action = Gio.SimpleAction.new("quit", None)
+        close_action.connect("activate", lambda x, y: self.quit())
+        self.add_action(close_action)
+
     def do_activate(self) -> None:
         if not self.window:
             self.window = Gtk.ApplicationWindow(application=self, title=_("Local Services"), icon_name="blueman",
@@ -76,6 +80,10 @@ class BluemanServices(Gtk.Application):
             self.b_apply.connect("clicked", self.on_apply_clicked)
 
         self.window.present_with_time(Gtk.get_current_event_time())
+
+    def do_startup(self) -> None:
+        Gtk.Application.do_startup(self)
+        self.set_accels_for_action("app.quit", ["<Ctrl>w", "<Ctrl>q", "Escape"])
 
     def option_changed(self) -> None:
         rets = [plugin.on_query_apply_state() for plugin in ServicePlugin.instances if plugin._is_loaded]
