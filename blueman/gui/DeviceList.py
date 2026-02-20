6d5df6e9748a134ef3ee850889c6d9d888051ab9
Sander Sweers <infirit@gmail.com>
2026-02-20 15:48:52 +0100

    DeviceList: Use new find_adapter method
    This also fixes a bug where we call set_adapter() from set_adapter() and
    cause an AssertionError in ManagerMenu.py as Adapter is None. Reproducable
    by restarting the bluetooth service.

--- blueman/gui/DeviceList.py
+++ blueman/gui/DeviceList.py
@@ -9,7 +9,7 @@ from blueman.Constants import ICON_PATH
 from blueman.bluez.Manager import Manager
 from blueman.bluez.Device import Device, AnyDevice
 from blueman.bluez.Adapter import Adapter, AnyAdapter
-from blueman.bluez.errors import DBusNoSuchAdapterError, BluezDBusException
+from blueman.bluez.errors import BluezDBusException
 
 from gi.repository import GObject
 from gi.repository import GLib
@@ -176,34 +176,33 @@ class DeviceList(GenericList):
 
     #########################
 
-    def set_adapter(self, adapter: str | None = None) -> None:
+    def set_adapter(self, adapter: ObjectPath | str | None = None) -> None:
         self.clear()
         if self.discovering:
             self.stop_discovery()
             self.emit("adapter-property-changed", self.Adapter, ("Discovering", False))
 
         adapter = adapter_path_to_name(adapter)
-
-        logging.debug(f"Setting adapter to: {adapter}")
+        adapter_path: ObjectPath | None = None
+        emit_signal: bool = False
 
         if adapter is not None:
-            try:
-                self.Adapter = self.manager.get_adapter(adapter)
-                self.__adapter_path = self.Adapter.get_object_path()
-            except DBusNoSuchAdapterError:
-                logging.warning('Failed to set adapter, trying first available.')
-                self.set_adapter(None)
-                return
-        else:
-            adapters = self.manager.get_adapters()
-            if len(adapters) > 0:
-                self.Adapter = adapters[0]
-                self.__adapter_path = self.Adapter.get_object_path()
-            else:
-                self.Adapter = None
-                self.__adapter_path = None
+            adapter_path = self.manager.find_adapter(adapter)
+            if adapter_path is None:
+                logging.warning(f'Failed to find adapter {adapter}, trying first available.')
 
-        self.emit("adapter-changed", self.__adapter_path)
+        if adapter_path is None:
+            # Try and find any adapter
+            adapter_path = self.manager.find_adapter(None)
+
+        if adapter_path is not None:
+            logging.debug(f"Setting adapter to: {adapter_path_to_name(adapter_path)}")
+            emit_signal = adapter_path != self.__adapter_path
+            self.Adapter = Adapter(obj_path=adapter_path)
+            self.__adapter_path = adapter_path
+
+        if emit_signal:
+            self.emit("adapter-changed", self.__adapter_path)
 
     def update_progress(self, time: float, totaltime: float) -> bool:
         if not self.discovering:
